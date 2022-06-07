bfc12817f467cbd49599ff0e2da7b38498a1c969
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-07 16:42:12 +0530

    Updated file

--- src/main/java/com/module/objects/pim/configuration/OptionalFieldsObjects.java
+++ src/main/java/com/module/objects/pim/configuration/OptionalFieldsObjects.java
@@ -1,5 +1,33 @@
 package com.module.objects.pim.configuration;
 
-public class OptionalFieldsObjects {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.automation.base.BaseClass;
+
+public class OptionalFieldsObjects extends BaseClass {
+	
+	@FindBy(xpath="//input[@name='txtUsername']")
+	public WebElement username;
+	
+	@FindBy(xpath="//input[@id='txtPassword']")
+	public WebElement password;
+	
+	@FindBy(xpath="//input[@value='LOGIN']")
+	public WebElement login;
+	
+	@FindBy(xpath="//b[text()='PIM']")
+	public WebElement PIM;
+	
+	@FindBy(xpath="(//a[text()='Configuration'])[2]")
+	public WebElement configuration ;
+	
+	@FindBy(xpath="//a[text()='Optional Fields']")
+	public WebElement optional;
+	
+	
+	
+	
+
 
 }
