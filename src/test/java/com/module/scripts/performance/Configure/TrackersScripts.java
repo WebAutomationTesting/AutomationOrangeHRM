4739d17cab95f8dc70a6444b1d52610285cec762
HP <HP@DESKTOP-3D45S6U>
2022-06-07 17:13:59 +0530

    Updated TrackersScripts

--- src/test/java/com/module/scripts/performance/Configure/TrackersScripts.java
+++ src/test/java/com/module/scripts/performance/Configure/TrackersScripts.java
@@ -1,5 +1,26 @@
 package com.module.scripts.performance.Configure;
 
-public class TrackersScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.performance.Configure.TrackersUtils;
+
+public class TrackersScripts extends BaseClass{
+	
+	TrackersUtils trackersUtils;
+	
+	@BeforeClass
+	public void load() {
+		
+		trackersUtils=PageFactory.initElements(driver, TrackersUtils.class);
+	}
+	
+	@Test
+	public void logincredentials() {
+		
+		trackersUtils.applications("Admin", "admin123");
+	}
 
 }
