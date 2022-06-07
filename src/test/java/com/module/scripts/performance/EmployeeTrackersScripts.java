fb43ebe860cb32cd68efeda9d7c04c4fa7cff792
HP <HP@DESKTOP-3D45S6U>
2022-06-07 17:12:21 +0530

    Updated Employeetrackersscripts

--- src/test/java/com/module/scripts/performance/EmployeeTrackersScripts.java
+++ src/test/java/com/module/scripts/performance/EmployeeTrackersScripts.java
@@ -1,5 +1,25 @@
 package com.module.scripts.performance;
 
-public class EmployeeTrackersScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.performance.EmployeeTrackersUtils;
+
+public class EmployeeTrackersScripts extends BaseClass{
+	
+	EmployeeTrackersUtils employeeTrackersUtils;
+	
+	@BeforeClass
+	public void loan() {
+		employeeTrackersUtils=PageFactory.initElements(driver, EmployeeTrackersUtils.class);
+	}
+	
+	@Test
+	public void logincredentials() {
+		employeeTrackersUtils.applications("Admin", "admin123");
+	}
+	
 
 }
