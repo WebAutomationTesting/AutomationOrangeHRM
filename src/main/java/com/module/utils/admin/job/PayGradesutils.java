b46afb52c1a3d3f3ca936f67611e10b42a2a545a
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-07 15:15:34 +0530

    Updated utils

--- src/main/java/com/module/utils/admin/job/PayGradesutils.java
+++ src/main/java/com/module/utils/admin/job/PayGradesutils.java
@@ -1,5 +1,45 @@
 package com.module.utils.admin.job;
 
-public class PayGradesutils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.admin.job.PayGradesObjects;
+
+
+public class PayGradesutils extends BaseClass {
+	
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public PayGradesObjects payGradesObjects;
+	
+	public PayGradesutils() {
+		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		payGradesObjects=PageFactory.initElements(driver,PayGradesObjects.class);
+	}
+	
+	public void clickOnJob() {
+		seleniumUtills.clickOnElement(payGradesObjects.job);
+	}
+	
+	public void clickOnPayGrades() {
+		seleniumUtills.clickOnElement(payGradesObjects.payGrades);
+	}
+	
+	public void clickOnADD() {
+		seleniumUtills.clickOnElement(payGradesObjects.add);
+	}
+	
+	public void enterName(String name) {
+		seleniumUtills.entertext(payGradesObjects.name,name);
+	}
+	
+	public void clickOnSave() {
+		seleniumUtills.clickOnElement(payGradesObjects.save);
+	}
+	
+	
 
 }
