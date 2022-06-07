1064d4cf641305432fc90ee1f7a70058140adb3d
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-07 16:49:02 +0530

    Updated Utils

--- src/main/java/com/module/utils/pim/configuration/OptionalFieldsUtils.java
+++ src/main/java/com/module/utils/pim/configuration/OptionalFieldsUtils.java
@@ -1,5 +1,33 @@
 package com.module.utils.pim.configuration;
 
-public class OptionalFieldsUtils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.pim.configuration.OptionalFieldsObjects;
+
+public class OptionalFieldsUtils extends BaseClass {
+	
+	SeleniumUtills seleniumUtills;
+	SeleniumCore seleniumCore;
+	OptionalFieldsObjects optionalfieldsobjects;
+	
+	public OptionalFieldsUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		optionalfieldsobjects=PageFactory.initElements(driver,OptionalFieldsObjects.class);
+		
+	}
+	 public void applicationlogin( String username,String password) {
+		 
+		 seleniumUtills.entertext(optionalfieldsobjects.username,username);
+		 seleniumUtills.entertext(optionalfieldsobjects.password, password);
+		 seleniumUtills.clickOnElement(optionalfieldsobjects.login);
+		 seleniumUtills.clickOnElement(optionalfieldsobjects.PIM);
+		 seleniumUtills.clickOnElement(optionalfieldsobjects.configuration);
+		 seleniumUtills.clickOnElement(optionalfieldsobjects.optional);
+		 
+	 }
 
 }
