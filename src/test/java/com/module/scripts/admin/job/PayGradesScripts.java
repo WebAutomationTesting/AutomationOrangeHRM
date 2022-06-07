dc004d9021fbc4b30b09b89f55ec467f855675f7
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-07 15:16:40 +0530

    Scripts updated

--- src/test/java/com/module/scripts/admin/job/PayGradesScripts.java
+++ src/test/java/com/module/scripts/admin/job/PayGradesScripts.java
@@ -1,5 +1,36 @@
 package com.module.scripts.admin.job;
 
-public class PayGradesScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.objects.homepage.LoginLogoutpageObjects;
+import com.module.utils.admin.job.PayGradesutils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+
+public class PayGradesScripts extends BaseClass{
+	
+	PayGradesutils payGradesutils;
+	LoginLogoutpageObjects loginLogoutpageObject;
+	LoginLogoutpageUtils  loginLogoutpageUtils;
+	
+	@BeforeClass
+	public void load() {
+		payGradesutils=PageFactory.initElements(driver,PayGradesutils.class);
+		loginLogoutpageObject=PageFactory.initElements(driver,LoginLogoutpageObjects.class);	
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+	}
+	@Test
+	public void jobdetails() {
+		loginLogoutpageUtils.applicationURL("Admin","admin123");
+		loginLogoutpageUtils.clickOnAdmin();
+		loginLogoutpageUtils.clickOnUsermanagement();
+		payGradesutils.clickOnJob();
+		payGradesutils.clickOnPayGrades();
+		payGradesutils.clickOnADD();
+		payGradesutils.enterName("HRM");
+		payGradesutils.clickOnSave();
+	}
 
 }
