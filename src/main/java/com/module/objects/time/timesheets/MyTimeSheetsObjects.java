8f2f58741f0c5df8ecebda6d957b8f71dd78bdf8
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-07 16:23:19 +0530

    i had update MyTimesheetobjects

--- src/main/java/com/module/objects/time/timesheets/MyTimeSheetsObjects.java
+++ src/main/java/com/module/objects/time/timesheets/MyTimeSheetsObjects.java
@@ -1,5 +1,50 @@
 package com.module.objects.time.timesheets;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MyTimeSheetsObjects {
 
+	@FindBy(id="menu_time_viewTimeModule")
+	public WebElement time;
+
+	@FindBy(xpath = "//a[@id='menu_time_Timesheets']")
+	public WebElement timesheets;
+
+	@FindBy(xpath = "//a[text()='My Timesheets']")
+	public WebElement mytimesheet;
+
+	@FindBy(xpath = "//input[@id='btnEdit']")
+	public WebElement editbutton;
+
+	@FindBy(name="initialRows[0][projectName]")
+	public WebElement projectname;
+	
+	@FindBy(id = "initialRows_0_projectName")
+	public WebElement projectName;
+
+	@FindBy(xpath = "(//li[@class='ac_even ac_over'])[1]")
+	public WebElement selectProject;
+
+	@FindBy(xpath = "//select[@class='projectActivity']")
+	public WebElement Activityname;
+
+	@FindBy(xpath ="(//input[@class=' timeBox'])[1]")
+	public WebElement currentdate;
+
+	@FindBy(xpath = "//input[@id='submitSave']")
+	public WebElement save;
+
+	@FindBy(xpath = "//input[@name='btnAddRow']")
+	public WebElement addrow;
+
+	@FindBy(xpath = "//input[@name='btnRemoveRows']")
+	public WebElement removerows;
+
+	@FindBy(xpath = "//input[@value='Reset']")
+	public WebElement reset;
+
+	@FindBy(xpath = "//input[@id='btnBack']")
+	public WebElement cancel;
+	
 }
