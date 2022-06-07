fe36b378b2c98a4e562e71dbd606755a2abd11e7
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-07 16:50:01 +0530

    Updated Scripts

--- src/test/java/com/module/scripts/pim/OptionalFieldsScripts.java
+++ src/test/java/com/module/scripts/pim/OptionalFieldsScripts.java
@@ -1,5 +1,24 @@
 package com.module.scripts.pim;
 
-public class OptionalFieldsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.pim.configuration.OptionalFieldsUtils;
+
+public class OptionalFieldsScripts extends BaseClass {
+	
+	OptionalFieldsUtils optionalFieldsUtils;
+	
+	
+	@BeforeClass
+	public void load() {
+		optionalFieldsUtils=PageFactory.initElements(driver, OptionalFieldsUtils.class);
+	}
+	@Test
+	public void logincredits() {
+		optionalFieldsUtils.applicationlogin("Admin", "admin123");
+	}
 
 }
