790f62a39d444fd36de9e41907924f724ccb6c64
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-07 16:25:45 +0530

    i had update ConfigurationUtils

--- src/main/java/com/module/utils/time/attendance/ConfigurationUtils.java
+++ src/main/java/com/module/utils/time/attendance/ConfigurationUtils.java
@@ -1,5 +1,45 @@
 package com.module.utils.time.attendance;
 
-public class ConfigurationUtils {
+import java.security.PublicKey;
 
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.time.attendance.ConfigurationObjects;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+
+public class ConfigurationUtils extends BaseClass{
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public ConfigurationObjects configurationObjects;
+	
+	public ConfigurationUtils() {
+
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		configurationObjects=PageFactory.initElements(driver, ConfigurationObjects.class);
+	}
+	public void clickontime() {
+		seleniumUtills.clickOnElement(configurationObjects.time);
+	}
+	public void clickonattendence() {
+		seleniumUtills.clickOnElement(configurationObjects.attendence);
+	}
+	public void clickonconfiguration() {
+		seleniumUtills.clickOnElement(configurationObjects.configuration);
+	}
+	public void clickonattendenceconfiguration1() {
+		seleniumUtills.clickOnElement(configurationObjects.attendenceconfiguration1);
+	}
+	public void clickonattendenceconfiguration2() {
+		seleniumUtills.clickOnElement(configurationObjects.attendenceconfiguration2);
+	}
+	public void clickonattendenceconfiguration3() {
+		seleniumUtills.clickOnElement(configurationObjects.attendenceconfiguration3);
+	}
+	public void clickonsave() {
+		seleniumUtills.clickOnElement(configurationObjects.save);
+	}
 }
