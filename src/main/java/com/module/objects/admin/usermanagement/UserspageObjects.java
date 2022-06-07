a7f52d10d6bcc7bbcb67ae0966f1543b1dc20ac4
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-07 15:11:49 +0530

    Updated User objects

--- src/main/java/com/module/objects/admin/usermanagement/UserspageObjects.java
+++ src/main/java/com/module/objects/admin/usermanagement/UserspageObjects.java
@@ -23,4 +23,32 @@ public class UserspageObjects {
 	@FindBy(xpath = "//option[text()='Enabled']")
 	public WebElement enabled;
 	
+	@FindBy(xpath = "//input[@name='_search']")
+	public WebElement search;
+	
+	@FindBy(xpath = "//input[@value='Add']")
+	public WebElement add;
+	
+	@FindBy(xpath = "//select[@name='systemUser[userType]']")
+	public WebElement userroleSearch;
+	
+	@FindBy(xpath = "(//input[@type='text'])[1]")
+	public WebElement employeename;
+	
+	@FindBy(xpath = "(//input[@type='text'])[2]")
+	public WebElement userName;
+	
+	@FindBy(xpath = "//select[@name='systemUser[status]']")
+	public WebElement statusName;
+	
+	@FindBy(xpath = "(//input[@type='password'])[1]")
+	public WebElement password;
+	
+	@FindBy(xpath = "(//input[@type='password'])[2]")
+	public WebElement confirmPassword;
+	
+	@FindBy(xpath = "//input[@value='Save']")
+	public WebElement save;
+	
+	
 }
