9a9a949d266eab773569ace29c68842b9c904214
HP <HP@DESKTOP-0TKDIHU>
2022-06-07 16:00:08 +0530

    updated

--- src/main/java/com/module/utils/admin/organization/Locationsutils.java
+++ src/main/java/com/module/utils/admin/organization/Locationsutils.java
@@ -1,5 +1,41 @@
 package com.module.utils.admin.organization;
 
-public class Locationsutils {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+
+public class Locationsutils  extends BaseClass{
+	private static final WebElement username = null;
+	private static final WebElement password = null;
+	SeleniumCore seleniumCore;
+	SeleniumUtills seleniumUtills;
+	Locationsutils locationsutils;
+	
+	public Locationsutils( ) {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		locationsutils=PageFactory.initElements(driver, Locationsutils.class);
+	}
+	
+	public void applicationlogin(String username,String password) {
+		
+	}
+ public void enterusername(String username) {
+	 seleniumUtills.entertext(locationsutils.username, username);
+	 
+ }
+ public void enterpassword(String password) {
+	 seleniumUtills.entertext(locationsutils.password, password);
+ }
+ public void clickonelement() {
+	
+ }
+ 
+	
+
+ 
 }
+
