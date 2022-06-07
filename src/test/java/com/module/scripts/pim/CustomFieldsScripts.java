29a0c4a28ecff4ce70bec61bf7c1dca8cb972340
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-07 16:49:36 +0530

    Updated Scripts

--- src/test/java/com/module/scripts/pim/CustomFieldsScripts.java
+++ src/test/java/com/module/scripts/pim/CustomFieldsScripts.java
@@ -1,5 +1,34 @@
 package com.module.scripts.pim;
 
-public class CustomFieldsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.objects.homepage.LoginLogoutpageObjects;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.pim.configuration.CustomfieldsUtils;
+
+
+public class CustomFieldsScripts extends BaseClass {
+	
+	CustomfieldsUtils customfieldsUtils;
+	LoginLogoutpageObjects loginLogoutpageObjects;
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	
+	@BeforeClass
+	
+	public void data() {
+		customfieldsUtils=PageFactory.initElements(driver,CustomfieldsUtils.class );
+		loginLogoutpageObjects=PageFactory.initElements(driver,LoginLogoutpageObjects.class);
+		
+	}
+	
+	@Test
+	public void logincredits() {
+		
+		
+	}
+	
+	
 }
