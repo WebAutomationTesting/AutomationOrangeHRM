d998fbf63e79131d76eb7d9c9e3aa0fcfb3ab8b9
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-07 16:28:35 +0530

    I had update ConfigurationScripts

--- src/test/java/com/module/scripts/time/attendance/ConfigurationScripts.java
+++ src/test/java/com/module/scripts/time/attendance/ConfigurationScripts.java
@@ -1,5 +1,31 @@
 package com.module.scripts.time.attendance;
 
-public class ConfigurationScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.time.attendance.ConfigurationUtils;
+
+public class ConfigurationScripts extends BaseClass{
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	ConfigurationUtils configurationUtils;
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		configurationUtils=PageFactory.initElements(driver, ConfigurationUtils.class);
+	}
+	@Test
+	
+public void timeattendance() {
+	loginLogoutpageUtils.applicationURL("Admin","admin123");
+	configurationUtils.clickontime();
+	configurationUtils.clickonattendence();
+	configurationUtils.clickonconfiguration();
+	configurationUtils.clickonattendenceconfiguration1();
+	configurationUtils.clickonattendenceconfiguration2();
+	configurationUtils.clickonattendenceconfiguration3();
+	configurationUtils.clickonsave();
+}
 }
