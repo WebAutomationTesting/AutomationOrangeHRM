89550c46be0f47ad037f50226cd7afe3c82c007f
HP <HP@DESKTOP-3D45S6U>
2022-06-07 17:13:19 +0530

    Updated KpisScripts

--- src/test/java/com/module/scripts/performance/Configure/KPlsScripts.java
+++ src/test/java/com/module/scripts/performance/Configure/KPlsScripts.java
@@ -1,5 +1,26 @@
 package com.module.scripts.performance.Configure;
 
-public class KPlsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.performance.Configure.KPlsUtils;
+
+public class KPlsScripts extends BaseClass{
+	
+	KPlsUtils KPlsUtils;
+
+	@BeforeClass
+	public void load() {
+		
+		KPlsUtils=PageFactory.initElements(driver, KPlsUtils.class);
+	}
+	
+	@Test
+	public void logincredentials() {
+		
+		KPlsUtils.apllications("Admin", "admin123");
+	}
 
 }
