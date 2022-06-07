f9aa619602c4f9cd1ad3d3159e4f7c50f5bc67e3
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-07 16:41:31 +0530

    Updated code

--- src/main/java/com/module/objects/pim/configuration/CustomFieldsObjects.java
+++ src/main/java/com/module/objects/pim/configuration/CustomFieldsObjects.java
@@ -1,5 +1,33 @@
 package com.module.objects.pim.configuration;
 
-public class CustomFieldsObjects {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.automation.base.BaseClass;
+
+public class CustomFieldsObjects extends BaseClass {
+
+
+
+	@FindBy(xpath="//b[text()='PIM']")
+	public WebElement PIM;
+
+	@FindBy(xpath="(//a[text()='Configuration'])[2]")
+	public WebElement configuration ;
+
+	@FindBy(xpath="//a[text()='Custom Fields']")
+	public WebElement custom;
+
+	@FindBy(xpath="//input[@value='Add']")
+	public WebElement add;
+
+	@FindBy(xpath="(//input[@type='text'])[1]")
+	public WebElement enter;
+
+	@FindBy(xpath="//select[@class='valid']")
+	public WebElement job;
+
+
+
 
 }
