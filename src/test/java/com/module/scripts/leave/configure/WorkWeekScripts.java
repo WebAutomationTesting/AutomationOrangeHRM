5d5d0ad94ecede805dd785e897e237f6a69e4a38
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-07 15:28:31 +0530

    Updated file

--- src/test/java/com/module/scripts/leave/configure/WorkWeekScripts.java
+++ src/test/java/com/module/scripts/leave/configure/WorkWeekScripts.java
@@ -1,5 +1,37 @@
 package com.module.scripts.leave.configure;
 
-public class WorkWeekScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.objects.homepage.LoginLogoutpageObjects;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.leave.configure.WorkWeekUtils;
+
+public class WorkWeekScripts extends BaseClass{
+	
+	WorkWeekUtils workWeekUtils;
+	LoginLogoutpageObjects loginLogoutpageObjects;
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	
+	@BeforeClass
+	public void load() {
+		
+		workWeekUtils=PageFactory.initElements(driver, WorkWeekUtils.class);
+		loginLogoutpageObjects=PageFactory.initElements(driver, LoginLogoutpageObjects.class);
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		
+	}
+	
+	@Test
+	public void LoginCredentials() {
+		loginLogoutpageUtils.applicationURL("Admin", "admin123");
+		workWeekUtils.selectLeave();
+		workWeekUtils.selectConfigure();
+		workWeekUtils.selectWokWeek();
+		workWeekUtils.selectEdit();
+		
+	}
 
 }
