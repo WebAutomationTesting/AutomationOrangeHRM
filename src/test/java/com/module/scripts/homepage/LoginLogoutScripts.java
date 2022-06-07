541478d5350b23fd5b212dc82842b86309d9e36c
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-07 16:27:45 +0530

    I had update LoginLogoutScripts

--- src/test/java/com/module/scripts/homepage/LoginLogoutScripts.java
+++ src/test/java/com/module/scripts/homepage/LoginLogoutScripts.java
@@ -1,6 +1,7 @@
 package com.module.scripts.homepage;
 
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -8,16 +9,16 @@ import com.automation.base.BaseClass;
 import com.module.utils.homepage.LoginLogoutpageUtils;
 
 public class LoginLogoutScripts extends BaseClass{
-	
+
 	LoginLogoutpageUtils loginLogoutpageUtils;
-	
+
 	@BeforeClass
-	
+
 	public void load() {
 		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
-		
+
 	}
-	
+
 	@Test
 	public void logindetails() {
 		loginLogoutpageUtils.applicationURL("Admin","admin123");
@@ -25,7 +26,10 @@ public class LoginLogoutScripts extends BaseClass{
 		loginLogoutpageUtils.clickOnUsermanagement();
 		loginLogoutpageUtils.clickOnUser();
 	}
-	
-	
 
+	@AfterMethod
+	public void logout() {
+		loginLogoutpageUtils.applicationlogout();
+	}
 }
+
