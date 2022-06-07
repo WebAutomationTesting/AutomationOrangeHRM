0f49bb88c7661bede6e12623bf15c58ecf1533da
HP <HP@DESKTOP-3D45S6U>
2022-06-07 17:10:36 +0530

    KpisObjects

--- src/main/java/com/module/utils/performance/Configure/KPlsUtils.java
+++ src/main/java/com/module/utils/performance/Configure/KPlsUtils.java
@@ -1,5 +1,33 @@
 package com.module.utils.performance.Configure;
 
-public class KPlsUtils {
+import java.util.Base64;
 
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.performance.Configure.KPlsObjects;
+
+public class KPlsUtils extends BaseClass{
+	
+	SeleniumCore seleniumCore;
+	SeleniumUtills seleniumUtills;
+	KPlsObjects kpisobjects;
+
+	public KPlsUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		kpisobjects=PageFactory.initElements(driver, KPlsObjects.class);
+	}
+
+	public void apllications(String username, String password) {
+	
+		seleniumUtills.clickOnElement(kpisobjects.Performance);
+		seleniumUtills.clickOnElement(kpisobjects.Configure);
+		seleniumUtills.clickOnElement(kpisobjects.KPIs);
+		//seleniumUtills.clickOnElement(kpisobjects.All);
+		seleniumUtills.clickOnElement(kpisobjects.QAEngineer);
+		seleniumUtills.clickOnElement(kpisobjects.Search);
+	}
 }
