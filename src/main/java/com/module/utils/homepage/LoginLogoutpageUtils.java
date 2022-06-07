3c6cb6a9b077dfd13fa2f2cbda6236ef4ee59567
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-07 16:24:57 +0530

    i had update LoginLogoutpageutils

--- src/main/java/com/module/utils/homepage/LoginLogoutpageUtils.java
+++ src/main/java/com/module/utils/homepage/LoginLogoutpageUtils.java
@@ -8,43 +8,43 @@ import com.automation.utills.SeleniumUtills;
 import com.module.objects.homepage.LoginLogoutpageObjects;
 
 public class LoginLogoutpageUtils extends BaseClass{
-	
+
 	public SeleniumUtills seleniumUtills;
 	public SeleniumCore seleniumCore;
 	public LoginLogoutpageObjects loginLogoutpageObject;
-	
-	
+
+
 	public LoginLogoutpageUtils() {
-		
 		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
 		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
 		loginLogoutpageObject=PageFactory.initElements(driver,LoginLogoutpageObjects.class);
-				
 	}
-	
+
 	public void applicationURL(String username,String password) {
 		seleniumUtills.entertext(loginLogoutpageObject.username,username);
 		seleniumUtills.entertext(loginLogoutpageObject.pass,password);
 		seleniumUtills.clickOnElement(loginLogoutpageObject.login);
-		
 	}
-	
+
 	public void clickOnAdmin() {
 		seleniumUtills.clickOnElement(loginLogoutpageObject.admin);
 	}
-	
+
 	public void clickOnUsermanagement() {
 		seleniumUtills.clickOnElement(loginLogoutpageObject.usermanagement);
 	}
-	
-    public void clickOnUser() {		
-		seleniumUtills.clickOnElement(loginLogoutpageObject.user);
-		
-	}
-    
-    
-	
-}
 
+	public void clickOnUser() {		
+		seleniumUtills.clickOnElement(loginLogoutpageObject.user);
+
+	}
+	
+	public void applicationlogout() {
+		seleniumUtills.clickOnElement(loginLogoutpageObject.tologout);
+		seleniumUtills.clickOnElement(loginLogoutpageObject.logout);   	
+	}
+
+
+}
 
 
