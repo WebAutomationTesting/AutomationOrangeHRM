578f49e14bc653025cd3fee2166d185f5a2030ba
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-07 16:29:15 +0530

    I had update MyTimesheetsScripts

--- src/test/java/com/module/scripts/time/timesheets/MyTimeSheetsScripts.java
+++ src/test/java/com/module/scripts/time/timesheets/MyTimeSheetsScripts.java
@@ -1,5 +1,32 @@
 package com.module.scripts.time.timesheets;
 
-public class MyTimeSheetsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.time.timesheets.MyTimeSheetsUtils;
+
+public class MyTimeSheetsScripts extends BaseClass{
+	
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	MyTimeSheetsUtils myTimeSheetsUtils;
+	
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		myTimeSheetsUtils=PageFactory.initElements(driver, MyTimeSheetsUtils.class);  
+	}
+	
+	@Test
+	public void mytimesheets() {
+		loginLogoutpageUtils.applicationURL("Admin","admin123");
+		myTimeSheetsUtils.clickontime();
+		myTimeSheetsUtils.navigateMyTimesheet();
+		myTimeSheetsUtils.clickOnEditButton();
+		myTimeSheetsUtils.enterProjectName("Global Corp and Co - Global Software phase - 1");
+		//myTimeSheetsUtils.selectonActivityname("");
+	}
 
 }
