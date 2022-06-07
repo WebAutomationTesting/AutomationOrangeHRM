2d70e6bb21868cdd1e5612c4e3146232b36a3c55
HP <HP@DESKTOP-0TKDIHU>
2022-06-07 16:01:31 +0530

    updated

--- src/test/java/com/module/scripts/admin/organization/Locationsscripts.java
+++ src/test/java/com/module/scripts/admin/organization/Locationsscripts.java
@@ -1,5 +1,22 @@
 package com.module.scripts.admin.organization;
 
-public class Locationsscripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.utils.admin.organization.Locationsutils;
+
+public class Locationsscripts extends BaseClass {
+	
+	Locationsutils locationsutils;
+	@BeforeClass
+	public void load() {
+		locationsutils=PageFactory.initElements(driver, Locationsutils.class);
+	}
+  @Test
+  public void logindetails() {
+	  locationsutils.applicationlogin("Admin", "admin123");
+	  
+  }
 }
