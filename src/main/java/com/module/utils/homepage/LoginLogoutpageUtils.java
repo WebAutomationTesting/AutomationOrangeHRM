c11b2b01e67f72bcde4e2b3456de47ca65882924
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-08 12:00:34 +0530

    Updated methods

--- src/main/java/com/module/utils/homepage/LoginLogoutpageUtils.java
+++ src/main/java/com/module/utils/homepage/LoginLogoutpageUtils.java
@@ -5,22 +5,25 @@ import org.openqa.selenium.support.PageFactory;
 import com.automation.base.BaseClass;
 import com.automation.utills.SeleniumCore;
 import com.automation.utills.SeleniumUtills;
+import com.automation.utills.WaitUtils;
 import com.module.objects.homepage.LoginLogoutpageObjects;
 
 public class LoginLogoutpageUtils extends BaseClass{
 
 	public SeleniumUtills seleniumUtills;
 	public SeleniumCore seleniumCore;
+	WaitUtils waitUtils;
 	public LoginLogoutpageObjects loginLogoutpageObject;
 
 
 	public LoginLogoutpageUtils() {
 		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
 		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
 		loginLogoutpageObject=PageFactory.initElements(driver,LoginLogoutpageObjects.class);
 	}
 
-	public void applicationURL(String username,String password) {
+	public void applicationCredentials(String username,String password) {
 		seleniumUtills.entertext(loginLogoutpageObject.username,username);
 		seleniumUtills.entertext(loginLogoutpageObject.pass,password);
 		seleniumUtills.clickOnElement(loginLogoutpageObject.login);
@@ -41,10 +44,9 @@ public class LoginLogoutpageUtils extends BaseClass{
 	
 	public void applicationlogout() {
 		seleniumUtills.clickOnElement(loginLogoutpageObject.tologout);
+		waitUtils.waitTime(500);
 		seleniumUtills.clickOnElement(loginLogoutpageObject.logout);   	
 	}
 
 
 }
-
-
