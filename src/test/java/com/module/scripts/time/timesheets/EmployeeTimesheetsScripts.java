34b695abe1cd98304e55d77ccbe00fc266eaf62b
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-08 12:04:34 +0530

    Updaetd

--- src/test/java/com/module/scripts/time/timesheets/EmployeeTimesheetsScripts.java
+++ src/test/java/com/module/scripts/time/timesheets/EmployeeTimesheetsScripts.java
@@ -1,5 +1,30 @@
 package com.module.scripts.time.timesheets;
 
-public class EmployeeTimesheetsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.time.timesheets.EmployeeTimesheetsUtils;
+
+public class EmployeeTimesheetsScripts extends BaseClass{
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	EmployeeTimesheetsUtils employeeTimesheetsUtils;
+	
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+        employeeTimesheetsUtils=PageFactory.initElements(driver, EmployeeTimesheetsUtils.class);
+
+    }
+	@Test
+	public void EmployeeTimesheet() {
+		loginLogoutpageUtils.applicationCredentials("Admin","admin123");
+		employeeTimesheetsUtils.clickontime();
+		employeeTimesheetsUtils.clickontimesheets();
+		employeeTimesheetsUtils.clickonEmployeeTimesheet();
+		employeeTimesheetsUtils.enterEmployeeName("Russel Hamilton");
+		employeeTimesheetsUtils.clickonview();
+	}
 }
--- src/test/java/com/module/scripts/time/timesheets/MyTimeSheetsScripts.java
+++ src/test/java/com/module/scripts/time/timesheets/MyTimeSheetsScripts.java
@@ -5,28 +5,33 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
 import com.module.utils.homepage.LoginLogoutpageUtils;
 import com.module.utils.time.timesheets.MyTimeSheetsUtils;
 
 public class MyTimeSheetsScripts extends BaseClass{
-	
+
 	LoginLogoutpageUtils loginLogoutpageUtils;
 	MyTimeSheetsUtils myTimeSheetsUtils;
-	
+	public WaitUtils waitUtils;
 	@BeforeClass
 	public void load() {
 		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
-		myTimeSheetsUtils=PageFactory.initElements(driver, MyTimeSheetsUtils.class);  
+		myTimeSheetsUtils=PageFactory.initElements(driver, MyTimeSheetsUtils.class); 
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
 	}
-	
+
 	@Test
 	public void mytimesheets() {
-		loginLogoutpageUtils.applicationURL("Admin","admin123");
+		loginLogoutpageUtils.applicationCredentials("Admin","admin123");
 		myTimeSheetsUtils.clickontime();
 		myTimeSheetsUtils.navigateMyTimesheet();
 		myTimeSheetsUtils.clickOnEditButton();
 		myTimeSheetsUtils.enterProjectName("Global Corp and Co - Global Software phase - 1");
-		//myTimeSheetsUtils.selectonActivityname("");
+		myTimeSheetsUtils.selectonActivityname(2);
+		
+		myTimeSheetsUtils.enterTimeondate("7");
+		myTimeSheetsUtils.clickonsave();
 	}
 
 }
