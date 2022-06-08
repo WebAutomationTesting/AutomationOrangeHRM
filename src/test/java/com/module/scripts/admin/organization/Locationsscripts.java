9baf53cefa3fc0c1dab9cf4fd0b189a1c122327d
HP <HP@DESKTOP-0TKDIHU>
2022-06-08 12:33:53 +0530

    updated location scripts

--- src/test/java/com/module/scripts/admin/organization/Locationsscripts.java
+++ src/test/java/com/module/scripts/admin/organization/Locationsscripts.java
@@ -6,17 +6,27 @@ import org.testng.annotations.Test;
 
 import com.automation.base.BaseClass;
 import com.module.utils.admin.organization.Locationsutils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
 
 public class Locationsscripts extends BaseClass {
 	
 	Locationsutils locationsutils;
+	LoginLogoutpageUtils loginLogoutpageUtils;
 	@BeforeClass
 	public void load() {
 		locationsutils=PageFactory.initElements(driver, Locationsutils.class);
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
 	}
   @Test
   public void logindetails() {
-	  locationsutils.applicationlogin("Admin", "admin123");
+	  loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+	  loginLogoutpageUtils.clickOnAdmin();
+	  locationsutils.selectorganization();
+	  locationsutils.selectlocation();
+	  locationsutils.entername("vrpalli");
+	  locationsutils.entercity("nellore");
+	  locationsutils.selectcountry();
+	  locationsutils.selectad();
 	  
   }
 }
