15a9ec2b33efd7272c78a5e939b6197a2e91e830
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-08 11:58:16 +0530

    Added new objects

--- src/main/java/com/module/objects/time/timesheets/EmployeeTimesheetsObjects.java
+++ src/main/java/com/module/objects/time/timesheets/EmployeeTimesheetsObjects.java
@@ -1,5 +1,25 @@
 package com.module.objects.time.timesheets;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class EmployeeTimesheetsObjects {
+	@FindBy(xpath = "//a[@id='menu_time_viewTimeModule']")
+	public WebElement time;
+
+	@FindBy(xpath = "//a[@id='menu_time_Timesheets']")
+	public WebElement timesheets;
+
+	@FindBy(xpath = "//a[text()='Employee Timesheets']")
+	public WebElement EmployeeTimesheet;
+
+	@FindBy(xpath = "//input[@name='time[employeeName]']")
+	public WebElement EmployeeName;
+
+	@FindBy(xpath = "//input[@id='btnView']")
+	public WebElement view;
+	
+	@FindBy(xpath="//a[text()='Add Timesheet']")
+	public WebElement AddTimesheet;
 
 }
