c046fb869198680c80e110fb91e35cb4b2e9d514
HP <HP@DESKTOP-0TKDIHU>
2022-06-08 09:13:23 +0530

    updated generalinfo

--- src/main/java/com/module/utils/admin/organization/Generalinformationutils.java
+++ src/main/java/com/module/utils/admin/organization/Generalinformationutils.java
@@ -8,46 +8,52 @@ import com.automation.utills.SeleniumUtills;
 import com.module.objects.admin.organization.GeneralinformationObjects;
 
 public class Generalinformationutils extends BaseClass {
-	
 	SeleniumCore seleniumCore;
 	SeleniumUtills seleniumUtills;
 	GeneralinformationObjects generalinformationObjects;
-	
+
 	public Generalinformationutils() {
 		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
 		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
 		generalinformationObjects=PageFactory.initElements(driver, GeneralinformationObjects.class);
 	}
-	
-	public void applicationlogin(String username,String password) {
-		
+	public void selectUserAdmin() {
+		seleniumUtills.clickOnElement(generalinformationObjects.Admin);
 	}
-	
-	public void selectusername() {
-		seleniumUtills.clickOnElement(generalinformationObjects.username);
-		
-	}
-		public void selectUserAdmin() {
-			//seleniumUtills.clickOnElement();
-		}
-		
-	public void clickonOrganization() {
+	public void selectorganization() {
 		seleniumUtills.clickOnElement(generalinformationObjects.Organization);
 	}
-	
-	
-	
-	
-	public void login(String enter) {
-		seleniumUtills.entertext(generalinformationObjects.orangehrm, enter);
+	public void selectgeneralinfo() { 
+		seleniumUtills.clickOnElement(generalinformationObjects.General);
 	}
-	
-	
+
+	public void selectedit() {
+		seleniumUtills.clickOnElement(generalinformationObjects.edit);
+	}
+
+	public void enterorganizationname(String organization) {
+		seleniumUtills.entertext(generalinformationObjects.orangehrm,organization);
+	}
+
+	public void enternumberofemployees( String numberofemployees) {
+		seleniumUtills.entertext(generalinformationObjects.numOfEmployees,numberofemployees);
+	}
+	public void enterPhonenumber(String phonenumber) {
+		seleniumUtills.entertext(generalinformationObjects.phonenumber,phonenumber );
+	}
+
+	public void enteremail( String email) {
+		seleniumUtills.entertext(generalinformationObjects.email, email);
+	}
+	public void entertaxid( String taxid) {
+		seleniumUtills.entertext(generalinformationObjects.taxId,taxid);
+	}
+	public void enterregistraionNumber( String registraionnumber) {
+		seleniumUtills.entertext(generalinformationObjects.registraionNumber, registraionnumber);
+	}
+	public void enterfax(String fax ) {
+		seleniumUtills.entertext(generalinformationObjects.fax, fax);
+	}
+
+
 }
-	
-	
-	
-	
-	
-
-
--- src/test/java/com/module/scripts/admin/organization/Generalinformationscripts.java
+++ src/test/java/com/module/scripts/admin/organization/Generalinformationscripts.java
@@ -1,5 +1,38 @@
 package com.module.scripts.admin.organization;
 
-public class Generalinformationscripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.objects.homepage.LoginLogoutpageObjects;
+import com.module.utils.admin.organization.Generalinformationutils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+
+public class Generalinformationscripts extends BaseClass {
+	Generalinformationutils generalinformationutils;
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	LoginLogoutpageObjects loginLogoutpageObjects;
+
+	@BeforeClass
+	public void load( ) {
+		generalinformationutils=PageFactory.initElements(driver,Generalinformationutils.class);
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		loginLogoutpageObjects=PageFactory.initElements(driver, LoginLogoutpageObjects.class);
+	}
+	@Test
+	public void logindetails() {
+		loginLogoutpageUtils.applicationURL("Admin","admin123");
+		generalinformationutils.selectUserAdmin();
+		generalinformationutils.selectorganization();
+		generalinformationutils.selectgeneralinfo();
+		generalinformationutils.selectedit();
+		generalinformationutils.enterorganizationname("smartshift");
+		generalinformationutils.enterPhonenumber("7569141938");
+		generalinformationutils.entertaxid("8796");
+		generalinformationutils.enteremail("malehari33@gmail.com");
+		generalinformationutils.enterregistraionNumber("ap9087");
+		generalinformationutils.enterfax("76543");
+
+	}
 }
