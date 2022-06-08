60c2299ee3a542ad444ac140241ca2ef13e298b8
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-08 11:56:16 +0530

    Added new method

--- src/main/java/com/automation/utills/SeleniumCore.java
+++ src/main/java/com/automation/utills/SeleniumCore.java
@@ -16,6 +16,13 @@ public class SeleniumCore extends BaseClass {
 		click(subMenuLocator).build().perform();
 	}
 	
+	public void clickOnSubSubmenu(WebElement menuLocator,WebElement subMenuLocator,WebElement subSubMenuLocator) {
+		actions=new Actions(driver);
+		actions.moveToElement(menuLocator).build().perform();
+		actions.moveToElement(subMenuLocator).build().perform();
+		actions.click(subSubMenuLocator).build().perform();
+	}
+	
 	public void clickOnEnterkeyBord() {
 		actions=new Actions(driver);
 		actions.sendKeys(Keys.ENTER).build().perform();
