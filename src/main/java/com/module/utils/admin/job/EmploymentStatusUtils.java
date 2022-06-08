0768100f8af0eca2b616c0199471a4d3bd164d13
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-08 12:43:11 +0530

    Updated EmploymentUtils

--- src/main/java/com/module/utils/admin/job/EmploymentStatusUtils.java
+++ src/main/java/com/module/utils/admin/job/EmploymentStatusUtils.java
@@ -1,5 +1,47 @@
 package com.module.utils.admin.job;
 
-public class EmploymentStatusUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.admin.job.EmploymentStatusObjects;
+
+
+public class EmploymentStatusUtils extends BaseClass {
+	
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public EmploymentStatusObjects employmentStatusObjects;
+
+	public EmploymentStatusUtils() {
+		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		employmentStatusObjects=PageFactory.initElements(driver,EmploymentStatusObjects.class);
+	}
+	
+	public void clickOnEmployment() {
+		seleniumUtills.clickOnElement(employmentStatusObjects.employment);
+	}
+	
+	public void clickOnEmploymentStatus() {
+		seleniumUtills.clickOnElement(employmentStatusObjects.employmentStatus);
+	}
+	
+	public void enterEmploymentStatusName(String employmentStatusName) {
+		seleniumUtills.entertext(employmentStatusObjects.employmentStatusName,employmentStatusName);
+	}
+	
+	public void clickOnSave() {
+		seleniumUtills.clickOnElement(employmentStatusObjects.save);
+	}
+	
+	public void clickOnPermanent() {
+		seleniumUtills.clickOnElement(employmentStatusObjects.permanent);
+	}
+	
+	public void clickOnAdd() {
+		seleniumUtills.clickOnElement(employmentStatusObjects.add);
+	}
+	
 }
