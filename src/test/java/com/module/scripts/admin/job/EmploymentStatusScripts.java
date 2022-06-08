ce747369843b11f8e786a10636a44c2079ded301
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-08 12:44:36 +0530

    Updated Scripts

--- src/test/java/com/module/scripts/admin/job/EmploymentStatusScripts.java
+++ src/test/java/com/module/scripts/admin/job/EmploymentStatusScripts.java
@@ -1,5 +1,40 @@
 package com.module.scripts.admin.job;
 
-public class EmploymentStatusScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.objects.homepage.LoginLogoutpageObjects;
+import com.module.utils.admin.job.EmploymentStatusUtils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+
+public class EmploymentStatusScripts extends BaseClass {
+	
+
+	EmploymentStatusUtils employmentStatusUtils;
+	LoginLogoutpageObjects loginLogoutpageObject;
+	LoginLogoutpageUtils  loginLogoutpageUtils;
+
+	@BeforeClass
+	public void load() {
+		employmentStatusUtils=PageFactory.initElements(driver,EmploymentStatusUtils.class);
+		loginLogoutpageObject=PageFactory.initElements(driver,LoginLogoutpageObjects.class);	
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+	}
+	
+	@Test
+	public void Employmentdetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin","admin123");
+		loginLogoutpageUtils.clickOnAdmin();
+		loginLogoutpageUtils.clickOnUsermanagement();
+		loginLogoutpageUtils.clickOnJob();
+		employmentStatusUtils.clickOnEmployment();
+		employmentStatusUtils.clickOnAdd();
+		employmentStatusUtils.enterEmploymentStatusName("Full-Time Permanent");
+		employmentStatusUtils.clickOnSave();
+		employmentStatusUtils.clickOnPermanent();
+		
+	}
 
 }
