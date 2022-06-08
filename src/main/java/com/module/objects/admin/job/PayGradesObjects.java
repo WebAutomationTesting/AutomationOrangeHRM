d51ec6805c3c5b7ecb9b74370a5163fabbf74071
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-08 12:41:04 +0530

    Updated PayGrades Objects

--- src/main/java/com/module/objects/admin/job/PayGradesObjects.java
+++ src/main/java/com/module/objects/admin/job/PayGradesObjects.java
@@ -4,20 +4,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class PayGradesObjects {
-	 
+
 	@FindBy(xpath = "//a[text()='Job']")
 	public WebElement job;
-	
+
 	@FindBy(xpath = "//a[text()='Pay Grades']")
 	public WebElement payGrades;
-	
+
 	@FindBy(xpath = "//input[@value='Add']")
 	public WebElement add;
-	
+
 	@FindBy(xpath = "//input[@name='payGrade[name]']")
 	public WebElement name;
-	
+
 	@FindBy(xpath = "//input[@value='Save']")
 	public WebElement save;
+
+	@FindBy(xpath = "//input[@name='payGradeCurrency[currencyName]']")
+	public WebElement currency;
+
+	@FindBy(xpath = "//input[@name='payGradeCurrency[minSalary]']")
+	public WebElement minSalary;
+
+	@FindBy(xpath = "//input[@name='payGradeCurrency[maxSalary]']")
+	public WebElement maxSalary;
 
 }
