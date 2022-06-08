8cb4e870e2746784c017ef451ac3f976f8152dad
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-08 11:57:55 +0530

    Added new objects

--- src/main/java/com/module/objects/pim/AddEmployeeobjects.java
+++ src/main/java/com/module/objects/pim/AddEmployeeobjects.java
@@ -1,5 +1,45 @@
 package com.module.objects.pim;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AddEmployeeobjects {
+	
+	@FindBy(id="menu_pim_viewPimModule")
+	public WebElement pimHeader;
+	
+	@FindBy(id="menu_pim_addEmployee")
+	public WebElement addEmployeeLink;
+	
+	@FindBy(id="firstName")
+	public WebElement firstName;
+	
+	@FindBy(id="middleName")
+	public WebElement middleName;
+	
+	@FindBy(id="lastName")
+	public WebElement lastName;
+	
+	@FindBy(id="photofile")
+	public WebElement photo;
+	
+	@FindBy(id="employeeId")
+	public WebElement empID;
+	
+	@FindBy(id="btnSave")
+	public WebElement saveButton;
+	
+	@FindBy(id="chkLogin")
+	public WebElement createLoginDetails;
+	
+	@FindBy(id="user_name")
+	public WebElement username;
+	
+	@FindBy(id="user_password")
+	public WebElement password;
+	
+	@FindBy(id="re_password")
+	public WebElement confirmPassword;
+	
 
 }
