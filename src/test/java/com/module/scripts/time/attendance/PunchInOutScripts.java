5fbe51557c2b5ccbc256f16832e958ebb9a692b1
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-09 16:31:43 +0530

    i had updated PunchinoutScripts

--- src/test/java/com/module/scripts/time/attendance/PunchInOutScripts.java
+++ src/test/java/com/module/scripts/time/attendance/PunchInOutScripts.java
@@ -1,5 +1,35 @@
 package com.module.scripts.time.attendance;
 
-public class PunchInOutScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+
+
+import com.automation.utills.WaitUtils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.time.attendance.PunchInOutUtils;
+
+public class PunchInOutScripts extends BaseClass {
+
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	WaitUtils waitUtils;
+	PunchInOutUtils punchInOutUtils;
+
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
+		punchInOutUtils=PageFactory.initElements(driver, PunchInOutUtils.class);
+	}
+
+	@Test
+	public void punchinoutemployeedetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin","admin123");
+		punchInOutUtils.clickOnPunchInOut();
+		punchInOutUtils.punIn("Employee In");
+		punchInOutUtils.punchout("Employee out");
+	}
 
 }
