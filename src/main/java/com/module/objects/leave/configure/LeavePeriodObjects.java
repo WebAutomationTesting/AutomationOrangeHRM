6ab484c81d90cf82a59b921b9d1b5129d3691db5
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-09 17:54:08 +0530

    Update

--- src/main/java/com/module/objects/leave/configure/LeavePeriodObjects.java
+++ src/main/java/com/module/objects/leave/configure/LeavePeriodObjects.java
@@ -1,5 +1,40 @@
 package com.module.objects.leave.configure;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LeavePeriodObjects {
+
+
+	@FindBy(xpath = "//b[text()='Leave']")
+	public WebElement leaveMenu;
+
+	@FindBy(xpath = "(//a[text()='Configure'])[1]")
+	public WebElement configuresubMenu;
+
+	@FindBy(xpath = "//a[text()='Leave Period']")
+	public WebElement leaveperiodsubsubMenu;
+
+	@FindBy(id="btnEdit")
+	public WebElement edit;
+	
+	@FindBy(id="leaveperiod_cmbStartMonth")
+	public WebElement month;
+	
+	@FindBy(xpath="//option[text()='March'")
+	public WebElement smonth;
+	
+	@FindBy(id="leaveperiod_cmbStartDate")
+	public WebElement date;
+	
+	@FindBy(xpath="//option[text()='30']")
+	public WebElement sdate;
+	
+	@FindBy(id="btnEdit")
+	public WebElement save;
+
+	
+	
+	
 
 }
