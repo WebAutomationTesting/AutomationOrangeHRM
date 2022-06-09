b855a8764696b992393b1c161bf4c87802969be4
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:29:21 +0530

    Updated Scripts

--- src/test/java/com/module/scripts/pim/TerminationReasonsScripts.java
+++ src/test/java/com/module/scripts/pim/TerminationReasonsScripts.java
@@ -1,5 +1,32 @@
 package com.module.scripts.pim;
 
-public class TerminationReasonsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.pim.configuration.TerminationReasonsUtils;
+
+public class TerminationReasonsScripts extends BaseClass {
+	
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	TerminationReasonsUtils terminationReasonsUtils;
+	
+	@BeforeClass
+	public void loadClass() {
+		loginLogoutpageUtils=PageFactory.initElements(driver,LoginLogoutpageUtils.class);
+		terminationReasonsUtils=PageFactory.initElements(driver,TerminationReasonsUtils.class);
+	}
+	@Test
+	public void logincredits() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		terminationReasonsUtils.clickOnPim();
+		terminationReasonsUtils.clickOnCofiguration();
+		terminationReasonsUtils.clickOnTermination();
+		terminationReasonsUtils.clickOnAdd();
+		terminationReasonsUtils.ClickOnUsername("Harish");
+		terminationReasonsUtils.clickOnSave();
+	}
 
 }
