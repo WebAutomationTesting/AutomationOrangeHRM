13c9e31c2c77ad6fad6fd0aff2f09765f45ea749
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:15:59 +0530

    Updated directory objects

--- src/main/java/com/module/objects/directory/Directoryobjects.java
+++ src/main/java/com/module/objects/directory/Directoryobjects.java
@@ -1,5 +1,19 @@
 package com.module.objects.directory;
 
-public class Directoryobjects {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.automation.base.BaseClass;
+
+public class Directoryobjects extends BaseClass {
+
+	@FindBy(xpath="//b[text()='Directory']")
+	public WebElement directory;
+
+	@FindBy(id="searchDirectory_emp_name_empName")
+	public WebElement name;
+
+	@FindBy(id="searchBtn")
+	public WebElement search;
 
 }
