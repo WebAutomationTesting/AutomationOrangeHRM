7eef3c644d56b8fe9c2771ad9f32b570ff7abf32
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:17:34 +0530

    Updated scripts

--- src/test/java/com/module/scripts/admin/configuration/LanguagePackagesscripts.java
+++ src/test/java/com/module/scripts/admin/configuration/LanguagePackagesscripts.java
@@ -1,5 +1,38 @@
 package com.module.scripts.admin.configuration;
 
-public class LanguagePackagesscripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
+import com.module.utils.admin.configuration.LanguagePackagesUtils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+
+public class LanguagePackagesscripts extends BaseClass {
+
+	LanguagePackagesUtils languagePackagesUtils;
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	WaitUtils waitUtils;
+
+	@BeforeClass
+	public void loadclass() {
+		loginLogoutpageUtils=PageFactory.initElements(driver,LoginLogoutpageUtils.class);
+		waitUtils=PageFactory.initElements(driver,WaitUtils.class);
+		languagePackagesUtils=PageFactory.initElements(driver,LanguagePackagesUtils.class);
+	}
+	@Test
+
+	public void logindetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		languagePackagesUtils.clickadmin();
+		languagePackagesUtils.clickconfiguration();
+		languagePackagesUtils.clicklanguage();
+		languagePackagesUtils.clickedit();
+		languagePackagesUtils.selectLaunageDropdown("Aghem (Cameroon)");
+		languagePackagesUtils.clicksave();
+
+
+	}
 
 }
