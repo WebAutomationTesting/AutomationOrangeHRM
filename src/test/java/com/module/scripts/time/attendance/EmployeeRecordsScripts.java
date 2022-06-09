cd2b97f7df08be9ef4aa957224f6c6d4af4ade18
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-09 16:28:25 +0530

    update Employeerecordscripts

--- src/test/java/com/module/scripts/time/attendance/EmployeeRecordsScripts.java
+++ src/test/java/com/module/scripts/time/attendance/EmployeeRecordsScripts.java
@@ -1,5 +1,29 @@
 package com.module.scripts.time.attendance;
 
-public class EmployeeRecordsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.time.attendance.EmployeeRecordsUtils;
+
+
+public class EmployeeRecordsScripts extends BaseClass{
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	EmployeeRecordsUtils employeeRecordsUtils;
+	WaitUtils waitUtils;
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		employeeRecordsUtils=PageFactory.initElements(driver, EmployeeRecordsUtils.class);
+        waitUtils=PageFactory.initElements(driver,WaitUtils.class);
+	}
+	@Test
+	public void Employeerecorddetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin","admin123");
+		waitUtils.waitTime(500);
+
+}
 }
