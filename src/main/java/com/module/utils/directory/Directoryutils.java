552b2fcce593703be2a26d0f6d3921eca97cfec4
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:18:20 +0530

    Updated Directory Utils

--- src/main/java/com/module/utils/directory/Directoryutils.java
+++ src/main/java/com/module/utils/directory/Directoryutils.java
@@ -1,5 +1,33 @@
 package com.module.utils.directory;
 
-public class Directoryutils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.directory.Directoryobjects;
+
+
+public class Directoryutils extends BaseClass {
+
+	SeleniumCore seleniumCore;
+	SeleniumUtills seleniumUtills;
+	Directoryobjects directoryObjects;
+
+	public Directoryutils() {
+		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
+		directoryObjects=PageFactory.initElements(driver,Directoryobjects.class);
+	}
+
+	public void clickDirectory() {
+		seleniumUtills.clickOnElement(directoryObjects.directory);
+	}
+	public void searchname(String name) {
+		seleniumUtills.entertext(directoryObjects.name, name);
+	}
+	public void search() {
+		seleniumUtills.clickOnElement(directoryObjects.search);
+	}
 
 }
