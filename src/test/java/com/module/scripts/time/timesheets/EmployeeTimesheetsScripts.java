c427c2c32f07ce46596628524664565bce79b89f
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-09 16:40:47 +0530

    I had updated EmployeeTimesheetscripts

--- src/test/java/com/module/scripts/time/timesheets/EmployeeTimesheetsScripts.java
+++ src/test/java/com/module/scripts/time/timesheets/EmployeeTimesheetsScripts.java
@@ -5,26 +5,30 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
 import com.module.utils.homepage.LoginLogoutpageUtils;
 import com.module.utils.time.timesheets.EmployeeTimesheetsUtils;
 
 public class EmployeeTimesheetsScripts extends BaseClass{
 	LoginLogoutpageUtils loginLogoutpageUtils;
 	EmployeeTimesheetsUtils employeeTimesheetsUtils;
-	
+	WaitUtils waitUtils;
+
 	@BeforeClass
 	public void load() {
 		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
-        employeeTimesheetsUtils=PageFactory.initElements(driver, EmployeeTimesheetsUtils.class);
-
-    }
+		employeeTimesheetsUtils=PageFactory.initElements(driver, EmployeeTimesheetsUtils.class);
+		waitUtils=PageFactory.initElements(driver,WaitUtils.class);
+	}
 	@Test
 	public void EmployeeTimesheet() {
 		loginLogoutpageUtils.applicationCredentials("Admin","admin123");
-		employeeTimesheetsUtils.clickontime();
-		employeeTimesheetsUtils.clickontimesheets();
+		waitUtils.waitTime(500);
+		employeeTimesheetsUtils.clickOnTime();
 		employeeTimesheetsUtils.clickonEmployeeTimesheet();
-		employeeTimesheetsUtils.enterEmployeeName("Russel Hamilton");
-		employeeTimesheetsUtils.clickonview();
+		waitUtils.waitTime(500);
+		employeeTimesheetsUtils.enterEmployeeName("Russel Hamilton","dataApproved");
+
+
 	}
 }
