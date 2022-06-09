13625b4588ffd2e259fabf9dd43c58617e14b337
HP <HP@DESKTOP-3D45S6U>
2022-06-09 10:55:18 +0530

    Updated KPIsutils

--- src/main/java/com/module/utils/performance/Configure/KPlsUtils.java
+++ src/main/java/com/module/utils/performance/Configure/KPlsUtils.java
@@ -1,7 +1,5 @@
 package com.module.utils.performance.Configure;
 
-import java.util.Base64;
-
 import org.openqa.selenium.support.PageFactory;
 
 import com.automation.base.BaseClass;
@@ -10,10 +8,10 @@ import com.automation.utills.SeleniumUtills;
 import com.module.objects.performance.Configure.KPlsObjects;
 
 public class KPlsUtils extends BaseClass{
-	
-	SeleniumCore seleniumCore;
-	SeleniumUtills seleniumUtills;
-	KPlsObjects kpisobjects;
+
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public KPlsObjects kpisobjects;
 
 	public KPlsUtils() {
 		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
@@ -21,13 +19,46 @@ public class KPlsUtils extends BaseClass{
 		kpisobjects=PageFactory.initElements(driver, KPlsObjects.class);
 	}
 
-	public void apllications(String username, String password) {
-	
+	public void clickonPerformance() {
 		seleniumUtills.clickOnElement(kpisobjects.Performance);
+	}
+	public void clickonConfigure() {
 		seleniumUtills.clickOnElement(kpisobjects.Configure);
+	}
+	public void clickonKPIs() {
 		seleniumUtills.clickOnElement(kpisobjects.KPIs);
-		//seleniumUtills.clickOnElement(kpisobjects.All);
+	}
+	public void clickonQAEngineer() {
 		seleniumUtills.clickOnElement(kpisobjects.QAEngineer);
+	}
+	public void clickonSearch() {
 		seleniumUtills.clickOnElement(kpisobjects.Search);
 	}
+	public void clickonadD() {
+		seleniumUtills.clickOnElement(kpisobjects.adD);
+	}
+	public void clickonSelect() {
+		seleniumUtills.clickOnElement(kpisobjects.select);
+	}
+	public void selectQAE() {
+		seleniumUtills.clickOnElement(kpisobjects.QAE);
+	}
+	public void entertextKeypermance(String Name) {
+		seleniumUtills.entertext(kpisobjects.KeyPermance, "");
+	}
+	public void entertextMiniumRating(String Name) {
+		seleniumUtills.entertext(kpisobjects.MinimumRating, "");
+	}
+	public void entertextMaximumRating(String Name) {
+		seleniumUtills.entertext(kpisobjects.MiximumRating, "");
+	}
+	public void selectMakeDefaultScale() {
+		seleniumUtills.clickOnElement(kpisobjects.MakeDefaultScale);
+	}
+	public void clickonSaVe() {
+		seleniumUtills.clickOnElement(kpisobjects.SaVe);
+	}
+
+
 }
+
