29f2165b077906eb62bcf8c44132e79788a90602
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-09 17:58:49 +0530

    Update

--- src/main/java/com/module/objects/leave/ApplyObjects.java
+++ src/main/java/com/module/objects/leave/ApplyObjects.java
@@ -1,5 +1,15 @@
 package com.module.objects.leave;
 
-public class ApplyObjects {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class ApplyObjects {
+	
+	@FindBy(xpath="//b[text()='Leave']")
+	public WebElement leaveHeader;
+
+	@FindBy(xpath="//a[text()='Apply']")
+	public WebElement applylink;
+
+	
 }
