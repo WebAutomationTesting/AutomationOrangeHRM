51bf2973523496aaccf0ec49abc80dca547d46cb
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-09 17:57:45 +0530

    Update

--- src/test/java/com/module/scripts/leave/configure/LeavePeriodScripts.java
+++ src/test/java/com/module/scripts/leave/configure/LeavePeriodScripts.java
@@ -1,5 +1,38 @@
 package com.module.scripts.leave.configure;
 
-public class LeavePeriodScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
+import com.module.objects.homepage.LoginLogoutpageObjects;
+import com.module.utils.admin.job.EmploymentStatusUtils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.leave.configure.LeavePeriodUtils;
+
+public class LeavePeriodScripts extends BaseClass {
+	
+	public LeavePeriodUtils leavePeriodUtils;
+	public LoginLogoutpageUtils loginLogoutpageUtils;
+	
+	@BeforeClass
+	public void loadClass() {
+		leavePeriodUtils=PageFactory.initElements(driver,LeavePeriodUtils.class);
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		
+	}
+	
+	@Test
+	public void leavePeriod() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		leavePeriodUtils.clickOnLeaveperiod();
+		leavePeriodUtils.leavePeriodDetails();
+		leavePeriodUtils.selectStartMonth("March");
+		leavePeriodUtils.selectdate("31");
+		leavePeriodUtils.save();
+		
+		
+	}
+	
 }
