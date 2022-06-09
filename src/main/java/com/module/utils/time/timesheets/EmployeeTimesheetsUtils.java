97460bc6310968ab98d54232352e7f532f8353bb
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-09 16:38:48 +0530

    i had updated employeetimesheetutils

--- src/main/java/com/module/utils/time/timesheets/EmployeeTimesheetsUtils.java
+++ src/main/java/com/module/utils/time/timesheets/EmployeeTimesheetsUtils.java
@@ -14,33 +14,31 @@ public class EmployeeTimesheetsUtils extends BaseClass{
 	public SeleniumUtills seleniumUtills;
 	public WaitUtils waitUtils;
 	public EmployeeTimesheetsObjects employeeTimesheetsObjects;
+
 	public EmployeeTimesheetsUtils() {
-
-
 		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
 		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
 		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
 		employeeTimesheetsObjects=PageFactory.initElements(driver, EmployeeTimesheetsObjects.class);
 	}
 
-	public void clickontime() {
-		seleniumUtills.clickOnElement(employeeTimesheetsObjects.time);
-	}
-
-	public void clickontimesheets() {
-		seleniumUtills.clickOnElement(employeeTimesheetsObjects.timesheets);
+	public void clickOnTime() {
+		seleniumCore.clickOnSubmenu(employeeTimesheetsObjects.time,employeeTimesheetsObjects.timesheets);
 	}
 
 	public void clickonEmployeeTimesheet() {
 		seleniumUtills.clickOnElement(employeeTimesheetsObjects.EmployeeTimesheet);
 	}
-	public void enterEmployeeName(String name) {
+	public void enterEmployeeName(String name,String comments) {
 		seleniumUtills.entertext(employeeTimesheetsObjects.EmployeeName, name);
-		waitUtils.waitTime(1000);	
-	}
-	public void clickonview() {
+		waitUtils.waitTime(1000);
 		seleniumUtills.clickOnElement(employeeTimesheetsObjects.view);
+		seleniumUtills.clickOnElement(employeeTimesheetsObjects.submit);
+
+		seleniumUtills.entertext(employeeTimesheetsObjects.comment, comments);
+		seleniumUtills.clickOnElement(employeeTimesheetsObjects.approved);
 	}
+
 
 
 }
