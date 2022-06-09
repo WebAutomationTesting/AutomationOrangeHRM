1388f2bbc2c52293245df69e5c7ec52af61ff8c3
HP <HP@DESKTOP-3D45S6U>
2022-06-09 11:00:07 +0530

    Updated EmployeeTrackersUtils

--- src/main/java/com/module/utils/performance/EmployeeTrackersUtils.java
+++ src/main/java/com/module/utils/performance/EmployeeTrackersUtils.java
@@ -1,6 +1,5 @@
 package com.module.utils.performance;
 
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 import com.automation.base.BaseClass;
@@ -9,26 +8,26 @@ import com.automation.utills.SeleniumUtills;
 import com.module.objects.performance.employeetrackers.EmployeeTrackersObjects;
 
 public class EmployeeTrackersUtils extends BaseClass{
-	SeleniumUtills seleniumUtills;
-	SeleniumCore seleniumCore;
-	EmployeeTrackersObjects employeeTrackersObjects;
-	
+
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public EmployeeTrackersObjects employeeTrackersObjects;
+
 	public EmployeeTrackersUtils() {
 		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
 		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
 		employeeTrackersObjects=PageFactory.initElements(driver, EmployeeTrackersObjects.class);
 	}
-	
-	public void applications(String username,String password) {
-		seleniumUtills.entertext(employeeTrackersObjects.username, username);
-		seleniumUtills.entertext(employeeTrackersObjects.password, password);
-		seleniumUtills.clickOnElement(employeeTrackersObjects.login);
+
+	public void clickonElementPerformance(){
 		seleniumUtills.clickOnElement(employeeTrackersObjects.Performance);
-		seleniumUtills.clickOnElement(employeeTrackersObjects.Employetracker);
-		
 	}
-	
-	
+	public void clickonEmployetracker() {
+		seleniumUtills.clickOnElement(employeeTrackersObjects.Employetracker);
+
+	}
+
+
 
 
 }
