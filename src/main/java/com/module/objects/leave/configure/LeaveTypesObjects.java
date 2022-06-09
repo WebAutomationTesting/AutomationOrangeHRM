25255e3ffce108983e267e8017ebb4de65f44f2a
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-09 17:56:05 +0530

    Update

--- src/main/java/com/module/objects/leave/configure/LeaveTypesObjects.java
+++ src/main/java/com/module/objects/leave/configure/LeaveTypesObjects.java
@@ -1,5 +1,26 @@
 package com.module.objects.leave.configure;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LeaveTypesObjects {
+
+	@FindBy(xpath = "//b[text()='Leave']")
+	public WebElement leave;
+
+	@FindBy(xpath = "(//a[text()='Configure'])[1]")
+	public WebElement configure;
+
+	@FindBy(xpath = "//a[text()='Leave Types']")
+	public WebElement leavetypes;
+
+	@FindBy(xpath = "//input[@id='btnAdd']")
+	public WebElement add;
+
+	@FindBy(xpath = "//input[@type='text']")
+	public WebElement name;
+	
+	@FindBy(xpath = "//input[@name='saveButton']")
+	public WebElement save;
 
 }
