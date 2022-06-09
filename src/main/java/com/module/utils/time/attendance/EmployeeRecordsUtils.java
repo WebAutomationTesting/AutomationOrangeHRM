6a5e78777c14abd9b7ecc265546053201a1056a1
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-09 16:34:17 +0530

    I had updated EmployeeRecordUtils

--- src/main/java/com/module/utils/time/attendance/EmployeeRecordsUtils.java
+++ src/main/java/com/module/utils/time/attendance/EmployeeRecordsUtils.java
@@ -1,5 +1,27 @@
 package com.module.utils.time.attendance;
 
-public class EmployeeRecordsUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.automation.utills.WaitUtils;
+import com.module.objects.maintenance.purgerecords.EmployeeRecordsObjects;
+
+public class EmployeeRecordsUtils extends BaseClass{
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public WaitUtils waitUtils;
+    EmployeeRecordsObjects employeeRecordsObjects;
+    
+    public EmployeeRecordsUtils() {
+    	seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
+		employeeRecordsObjects=PageFactory.initElements(driver, EmployeeRecordsObjects.class);
+    }
+    
+    public void clickOnTime() {
+    
+}
 }
