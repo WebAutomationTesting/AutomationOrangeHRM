2b7f42ac9e6427e4e75629c2a6608ef1d7a37114
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-09 17:59:29 +0530

    Update

--- src/test/java/com/module/scripts/leave/ApplyScripts.java
+++ src/test/java/com/module/scripts/leave/ApplyScripts.java
@@ -1,5 +1,29 @@
 package com.module.scripts.leave;
 
-public class ApplyScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.leave.ApplyUtils;
+
+public class ApplyScripts extends BaseClass{
+	
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	ApplyUtils applyUtils;
+	
+	@BeforeClass
+	public void loadClass() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		applyUtils=PageFactory.initElements(driver, ApplyUtils.class);
+		
+	}
+
+	@Test
+	public void applyLeave() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		applyUtils.clickOnApply();
+		
+	}
 }
