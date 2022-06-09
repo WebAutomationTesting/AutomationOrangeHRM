c978cc992c238c6bd8f0b91ce65f90e990ab5324
HP <HP@DESKTOP-3D45S6U>
2022-06-09 10:57:35 +0530

    Updated KPIsScripts

--- src/test/java/com/module/scripts/performance/Configure/KPlsScripts.java
+++ src/test/java/com/module/scripts/performance/Configure/KPlsScripts.java
@@ -5,22 +5,39 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.automation.base.BaseClass;
+import com.module.objects.homepage.LoginLogoutpageObjects;
+import com.module.utils.homepage.LoginLogoutpageUtils;
 import com.module.utils.performance.Configure.KPlsUtils;
 
 public class KPlsScripts extends BaseClass{
-	
-	KPlsUtils KPlsUtils;
+
+	KPlsUtils kpisUtils;
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	LoginLogoutpageObjects loginLogoutpageObjects;
 
 	@BeforeClass
 	public void load() {
-		
-		KPlsUtils=PageFactory.initElements(driver, KPlsUtils.class);
+		kpisUtils=PageFactory.initElements(driver, KPlsUtils.class);
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		loginLogoutpageObjects=PageFactory.initElements(driver, LoginLogoutpageObjects.class);
 	}
-	
+
 	@Test
-	public void logincredentials() {
-		
-		KPlsUtils.apllications("Admin", "admin123");
+	public void KPIsdetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		kpisUtils.clickonPerformance();
+		kpisUtils.clickonConfigure();
+		kpisUtils.clickonKPIs();
+		kpisUtils.clickonQAEngineer();
+		kpisUtils.clickonSearch();
+		kpisUtils.clickonadD();
+		kpisUtils.clickonSelect();
+		kpisUtils.selectQAE();
+		kpisUtils.entertextKeypermance("");
+		kpisUtils.entertextMaximumRating("");
+		kpisUtils.entertextMaximumRating("");
+		kpisUtils.selectMakeDefaultScale();
+		kpisUtils.clickonSaVe();
 	}
 
 }
