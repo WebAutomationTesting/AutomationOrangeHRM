192bc739ce3cf94ffd7f0d249a725d5aac8cd113
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:14:40 +0530

    Updated language objects

--- src/main/java/com/module/objects/admin/configuration/LanguagePackagesObjects.java
+++ src/main/java/com/module/objects/admin/configuration/LanguagePackagesObjects.java
@@ -1,5 +1,31 @@
 package com.module.objects.admin.configuration;
 
-public class LanguagePackagesObjects {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.automation.base.BaseClass;
+
+public class LanguagePackagesObjects extends BaseClass {
+
+	@FindBy(xpath="//b[text()='Admin']")
+	public WebElement Admin;
+
+	@FindBy(id="menu_admin_Configuration")
+	public WebElement  configuration;
+
+	@FindBy(id="menu_admin_languagePackage")
+	public WebElement languages;
+
+	@FindBy(id="btnAdd")
+	public WebElement add;
+
+	@FindBy(id="addLanguagePackage_name")
+	public WebElement addlanguages;
+
+	@FindBy(xpath="//option[text()='Aghem (Cameroon)']")
+	public WebElement telugu;
+
+	@FindBy(id="btnSave")
+	public WebElement save;
 
 }
