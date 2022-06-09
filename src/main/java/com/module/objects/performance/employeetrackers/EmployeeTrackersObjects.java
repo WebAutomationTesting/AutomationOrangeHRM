b0b63752476e90b0c7ae2b0cf94d6bfbe4716c02
HP <HP@DESKTOP-3D45S6U>
2022-06-09 10:59:22 +0530

    Updated EmployeeTrackersObjects

--- src/main/java/com/module/objects/performance/employeetrackers/EmployeeTrackersObjects.java
+++ src/main/java/com/module/objects/performance/employeetrackers/EmployeeTrackersObjects.java
@@ -6,22 +6,11 @@ import org.openqa.selenium.support.FindBy;
 import com.automation.base.BaseClass;
 
 public class EmployeeTrackersObjects {
-	
-	@FindBy(xpath = "//input[@id='txtUsername']")
-	public WebElement username;
-	
-	@FindBy(xpath = "//input[@id='txtPassword']")
-	public WebElement password;
-	
-	@FindBy(xpath = "//input[@id='btnLogin']")
-	public WebElement login;
-	
+
 	@FindBy(xpath = "//a[@id='menu__Performance']")
 	public WebElement Performance;
-	
+
 	@FindBy(xpath = "//a[text()='Employee Trackers']")
 	public WebElement Employetracker;
-	
-	
 
 }
