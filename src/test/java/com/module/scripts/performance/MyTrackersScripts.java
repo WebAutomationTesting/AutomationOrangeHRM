79e50503439e6b31af8f6dee0816267a1fac5e8d
HP <HP@DESKTOP-3D45S6U>
2022-06-09 11:03:39 +0530

    Updated MyTrackersScripts

--- src/test/java/com/module/scripts/performance/MyTrackersScripts.java
+++ src/test/java/com/module/scripts/performance/MyTrackersScripts.java
@@ -1,5 +1,28 @@
 package com.module.scripts.performance;
 
-public class MyTrackersScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.performance.MyTrackersUtils;
+
+public class MyTrackersScripts extends BaseClass{
+
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	MyTrackersUtils myTrackersUtils;
+
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		myTrackersUtils=PageFactory.initElements(driver, MyTrackersUtils.class);
+	}
+	@Test
+	public void mytrackersDetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		myTrackersUtils.clickonPerformance();
+		myTrackersUtils.clickonmytrackers();
+	}
 
 }
