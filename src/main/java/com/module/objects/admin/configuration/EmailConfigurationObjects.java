9df6aa21066f0473ab31a2309e64f18913d3c1c5
HP <HP@DESKTOP-0TKDIHU>
2022-06-09 16:47:55 +0530

    updated email objects

--- src/main/java/com/module/objects/admin/configuration/EmailConfigurationObjects.java
+++ src/main/java/com/module/objects/admin/configuration/EmailConfigurationObjects.java
@@ -1,5 +1,38 @@
 package com.module.objects.admin.configuration;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class EmailConfigurationObjects {
+	
+	@FindBy(xpath = "//b[text()='Admin']")
+	public WebElement Admin;
+	
+	@FindBy(xpath = "//a[@id='menu_admin_Configuration']")
+	public WebElement Configuration;
+	
+	@FindBy(xpath = "//a[text()='Email Configuration']")
+	public WebElement emailconfirguration;
+	
+	@FindBy(xpath = "//input[@id='editBtn']")
+	public WebElement edit;
+	
+	@FindBy(id = "emailConfigurationForm_txtMailAddress")
+	public WebElement MailSentAs ;
+	
+	@FindBy(xpath = "//input[@type='checkbox']")
+	public WebElement checkbox;
+	
+	@FindBy(xpath = "//select[@id='emailConfigurationForm_cmbMailSendingMethod']")
+	public WebElement sendingmethod;
+	
+	@FindBy(xpath = "//option[@value='sendmail']")
+	public WebElement sendmail;
+	
+	@FindBy(xpath = "//input[@id='emailConfigurationForm_txtTestEmail']")
+	public WebElement testmail;
+	
+	@FindBy(xpath = "//input[@id='editBtn']")
+	public WebElement save;
 
 }
