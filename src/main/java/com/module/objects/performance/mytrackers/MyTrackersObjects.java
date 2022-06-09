ec871c485bf1fc4d94654afd971096564897cf0e
HP <HP@DESKTOP-3D45S6U>
2022-06-09 11:01:54 +0530

    Updated MyTrackersObjects

--- src/main/java/com/module/objects/performance/mytrackers/MyTrackersObjects.java
+++ src/main/java/com/module/objects/performance/mytrackers/MyTrackersObjects.java
@@ -1,5 +1,13 @@
 package com.module.objects.performance.mytrackers;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MyTrackersObjects {
 
+	@FindBy(xpath = "//a[@id='menu__Performance']")
+	public WebElement performance;
+
+	@FindBy(xpath = "//a[text()='My Trackers']")
+	public WebElement mytrackers;
 }
