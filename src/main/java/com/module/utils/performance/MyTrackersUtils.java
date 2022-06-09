78eeb250482a5efc3f1199f02e7f365e70af6d50
HP <HP@DESKTOP-3D45S6U>
2022-06-09 11:02:44 +0530

    Updated MyTrackersUtils

--- src/main/java/com/module/utils/performance/MyTrackersUtils.java
+++ src/main/java/com/module/utils/performance/MyTrackersUtils.java
@@ -1,5 +1,28 @@
 package com.module.utils.performance;
 
-public class MyTrackersUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.performance.mytrackers.MyTrackersObjects;
+
+public class MyTrackersUtils extends BaseClass{
+
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public MyTrackersObjects myTrackersObjects;
+
+	public MyTrackersUtils() {
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		myTrackersObjects=PageFactory.initElements(driver, MyTrackersObjects.class);
+	}
+
+	public void clickonPerformance() {
+		seleniumUtills.clickOnElement(myTrackersObjects.performance);
+	}
+	public void clickonmytrackers() {
+		seleniumUtills.clickOnElement(myTrackersObjects.mytrackers);
+	}
 }
