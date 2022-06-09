593cc7b8e8a1b539ce061b71c8a1e2dd531491a8
DELL <DELL@DESKTOP-LS0ISLU>
2022-06-09 16:36:41 +0530

    I had updated Punchinoututils

--- src/main/java/com/module/utils/time/attendance/PunchInOutUtils.java
+++ src/main/java/com/module/utils/time/attendance/PunchInOutUtils.java
@@ -1,5 +1,38 @@
 package com.module.utils.time.attendance;
 
-public class PunchInOutUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.automation.utills.WaitUtils;
+import com.module.objects.time.attendance.PunchInOutObjects;
+
+public class PunchInOutUtils extends BaseClass {
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public WaitUtils waitUtils;
+	PunchInOutObjects punchInOutObjects;
+
+	public PunchInOutUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
+		punchInOutObjects=PageFactory.initElements(driver, PunchInOutObjects.class);
+	}
+
+	public void clickOnPunchInOut() {
+		seleniumCore.clickOnSubSubmenu(punchInOutObjects.time, punchInOutObjects. attendence, punchInOutObjects.punchin);
+	}
+
+	public void punIn(String note) {
+		seleniumUtills.entertext(punchInOutObjects.PunchInOut,note);
+		seleniumUtills.clickOnElement(punchInOutObjects.in);
+
+	}
+
+	public void punchout(String note)  {
+		seleniumUtills.entertext(punchInOutObjects.PunchInOut,note);
+		seleniumUtills.clickOnElement(punchInOutObjects.out);
+	}
 }
