5e08d4796633577b2b175ff63d3d9040b0b7bb11
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:18:56 +0530

    Updated Directory Scripts

--- src/test/java/com/module/scripts/directory/Directoryscripts.java
+++ src/test/java/com/module/scripts/directory/Directoryscripts.java
@@ -1,5 +1,32 @@
 package com.module.scripts.directory;
 
-public class Directoryscripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
+import com.module.utils.directory.Directoryutils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+
+public class Directoryscripts extends BaseClass {
+
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	Directoryutils directoryutils;
+	WaitUtils waitUtils;
+
+	@BeforeClass
+	public void loadclass() {
+		loginLogoutpageUtils=PageFactory.initElements(driver,LoginLogoutpageUtils.class);
+		directoryutils=PageFactory.initElements(driver,Directoryutils.class);
+		waitUtils=PageFactory.initElements(driver,WaitUtils.class);
+	}
+	@Test
+	public void logincredits() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		directoryutils.clickDirectory();
+		directoryutils.searchname("Odis Adalwin");
+		directoryutils.search();
+	}
 
 }
