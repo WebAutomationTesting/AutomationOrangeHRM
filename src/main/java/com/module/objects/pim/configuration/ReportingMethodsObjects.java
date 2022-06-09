0a970d1adb6300772655df8c6046b7e5659da504
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:22:42 +0530

    Reporting method objects

--- src/main/java/com/module/objects/pim/configuration/ReportingMethodsObjects.java
+++ src/main/java/com/module/objects/pim/configuration/ReportingMethodsObjects.java
@@ -1,5 +1,29 @@
 package com.module.objects.pim.configuration;
 
-public class ReportingMethodsObjects {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.automation.base.BaseClass;
+
+public class ReportingMethodsObjects extends BaseClass {
+	
+	
+	@FindBy(xpath="//b[text()='PIM']")
+	public WebElement PIM;
+	
+	@FindBy(xpath="(//a[text()='Configuration'])[2]")
+	public WebElement configuration ;
+	
+	@FindBy(xpath="//a[text()='Reporting Methods']")
+	public WebElement reoprts;
+	
+	@FindBy(xpath="//input[@id='btnAdd']")
+	public WebElement add;
+	
+	@FindBy(xpath="//input[@name='reportingMethod[name]']")
+	public WebElement name;
+	
+	@FindBy(xpath="//input[@value='Save']")
+	public WebElement save;
 
 }
