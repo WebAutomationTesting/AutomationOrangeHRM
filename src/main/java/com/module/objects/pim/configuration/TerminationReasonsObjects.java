dc28552d75473563ef12fba92eeb39650dbd98c0
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:24:43 +0530

    Updated Termination

--- src/main/java/com/module/objects/pim/configuration/TerminationReasonsObjects.java
+++ src/main/java/com/module/objects/pim/configuration/TerminationReasonsObjects.java
@@ -1,5 +1,31 @@
 package com.module.objects.pim.configuration;
 
-public class TerminationReasonsObjects {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.automation.base.BaseClass;
+
+public class TerminationReasonsObjects extends BaseClass {
+	
+	@FindBy(xpath="//b[text()='PIM']")
+	public WebElement PIM;
+	
+	@FindBy(xpath="(//a[text()='Configuration'])[2]")
+	public WebElement configuration ;
+	
+	@FindBy(xpath="//a[text()='Termination Reasons']")
+	public WebElement termination;
+	
+	@FindBy(id="btnAdd")
+	public WebElement add;
+	
+	@FindBy(id="terminationReason_name")
+	public WebElement username;
+	
+	@FindBy(id="btnSave")
+	public WebElement save;
+	
+	
+	
 
 }
