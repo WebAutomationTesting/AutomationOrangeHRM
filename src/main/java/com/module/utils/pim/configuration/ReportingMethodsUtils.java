24bb4762cd25a9990b1ed9dd33662160a4cda4be
Admin <Admin@DESKTOP-SK2QLR7>
2022-06-09 17:26:55 +0530

    Updated Reporting

--- src/main/java/com/module/utils/pim/configuration/ReportingMethodsUtils.java
+++ src/main/java/com/module/utils/pim/configuration/ReportingMethodsUtils.java
@@ -1,5 +1,41 @@
 package com.module.utils.pim.configuration;
 
-public class ReportingMethodsUtils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.pim.configuration.ReportingMethodsObjects;
+
+public class ReportingMethodsUtils extends BaseClass {
+	
+	SeleniumUtills seleniumUtills;
+	SeleniumCore seleniumCore;
+	ReportingMethodsObjects reportingMethodsObjects;
+	
+	public ReportingMethodsUtils() {
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		reportingMethodsObjects=PageFactory.initElements(driver,ReportingMethodsObjects.class);
+	}
+	public void clickpim() {
+		seleniumUtills.clickOnElement(reportingMethodsObjects.PIM);
+	}
+	public void clickcon() {
+		seleniumUtills.clickOnElement(reportingMethodsObjects.configuration);
+	}
+	public void clickrep() {
+		seleniumUtills.clickOnElement(reportingMethodsObjects.reoprts);
+	}
+	public void clickadd() {
+		seleniumUtills.clickOnElement(reportingMethodsObjects.add);
+	}
+	public void entername(String name) {
+		seleniumUtills.entertext(reportingMethodsObjects.name, name);
+	}
+	public void clicksave() {
+		seleniumUtills.clickOnElement(reportingMethodsObjects.save);
+	}
+	
 
 }
