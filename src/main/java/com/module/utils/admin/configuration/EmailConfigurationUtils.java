40d5841d81677d8161202ec2d3afa6486280b230
HP <HP@DESKTOP-0TKDIHU>
2022-06-09 16:48:47 +0530

    updated email utils

--- src/main/java/com/module/utils/admin/configuration/EmailConfigurationUtils.java
+++ src/main/java/com/module/utils/admin/configuration/EmailConfigurationUtils.java
@@ -1,5 +1,56 @@
 package com.module.utils.admin.configuration;
 
-public class EmailConfigurationUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.admin.configuration.EmailConfigurationObjects;
+
+public class EmailConfigurationUtils extends BaseClass {
+	SeleniumCore seleniumCore;
+	SeleniumUtills seleniumUtills;
+	EmailConfigurationObjects emailConfigurationObjects;
+
+	public EmailConfigurationUtils() {
+		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		emailConfigurationObjects=PageFactory.initElements(driver, EmailConfigurationObjects.class);
+
+	}
+	public void selectAdmin() {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.Admin);
+	}
+	public void selectconfiguration( ) {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.Configuration);
+	}
+	public void selectemailconfiguration() {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.emailconfirguration);
+	}
+	public void selectedit() {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.edit);
+
+	}
+	public void entermailsentas(String sendmailas) {
+		seleniumUtills.entertext(emailConfigurationObjects.MailSentAs, sendmailas);
+
+	}
+	public void selectcheckbox() {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.checkbox);
+	}
+	public void selectsendingmethod() {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.sendingmethod);
+	}
+	
+	public void selectsendmail() {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.sendmail);
+	}
+	public void selecttestemailaddress() {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.testmail);
+	}
+	public void selectsave() {
+		seleniumUtills.clickOnElement(emailConfigurationObjects.save);
+	}
 }
+
+
