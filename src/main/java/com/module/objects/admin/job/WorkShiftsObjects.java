2a74f848be31c5ffb6d51c4440a67d2f5a310e98
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-10 12:56:41 +0530

    Updated objects

--- src/main/java/com/module/objects/admin/job/WorkShiftsObjects.java
+++ src/main/java/com/module/objects/admin/job/WorkShiftsObjects.java
@@ -1,5 +1,26 @@
 package com.module.objects.admin.job;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class WorkShiftsObjects {
+	
+	@FindBy(id="menu_admin_workShift")
+	public WebElement workShiftsTab;
+	
+	@FindBy(id="workShift_name")
+	public WebElement workShiftName;
+	
+	@FindBy(id="workShift_workHours_from")
+	public WebElement workFrom;
+	
+	@FindBy(xpath = "(//option[@value='06:00'])[1]")
+	public WebElement workOnTime;
+	
+	@FindBy(id="workShift_workHours_to")
+	public WebElement workTo;
+	
+	@FindBy(xpath="(//option[@value='18:00'])[1]")
+	public WebElement workOffTime;
 
 }
