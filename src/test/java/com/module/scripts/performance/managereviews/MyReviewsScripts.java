35cdfeefe130388d2da3897fdd7fc12a3c6e5152
HP <HP@DESKTOP-3D45S6U>
2022-06-10 15:22:13 +0530

    Updated MyReviewsScripts

--- src/test/java/com/module/scripts/performance/managereviews/MyReviewsScripts.java
+++ src/test/java/com/module/scripts/performance/managereviews/MyReviewsScripts.java
@@ -1,5 +1,29 @@
 package com.module.scripts.performance.managereviews;
 
-public class MyReviewsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.performance.managereviews.MyReviewsUtils;
+
+public class MyReviewsScripts extends BaseClass{
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	MyReviewsUtils myReviewsUtils;
+
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		myReviewsUtils=PageFactory.initElements(driver, MyReviewsUtils.class);
+	}
+
+	@Test
+	public void MyReviewsDetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		myReviewsUtils.clickonPerfomance();
+		myReviewsUtils.clickonManageReviews();
+		myReviewsUtils.clickonMyReviews();
+	}
 
 }
