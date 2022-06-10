2dc5e5de4738c2af4ee508bd556602a36d145502
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-10 13:02:17 +0530

    Updated

--- src/main/java/com/module/utils/admin/job/PayGradesutils.java
+++ src/main/java/com/module/utils/admin/job/PayGradesutils.java
@@ -9,37 +9,47 @@ import com.module.objects.admin.job.PayGradesObjects;
 
 
 public class PayGradesutils extends BaseClass {
-	
+
 	public SeleniumUtills seleniumUtills;
 	public SeleniumCore seleniumCore;
 	public PayGradesObjects payGradesObjects;
-	
+
 	public PayGradesutils() {
 		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
 		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
 		payGradesObjects=PageFactory.initElements(driver,PayGradesObjects.class);
 	}
-	
+
 	public void clickOnJob() {
 		seleniumUtills.clickOnElement(payGradesObjects.job);
 	}
-	
+
 	public void clickOnPayGrades() {
 		seleniumUtills.clickOnElement(payGradesObjects.payGrades);
 	}
-	
+
 	public void clickOnADD() {
 		seleniumUtills.clickOnElement(payGradesObjects.add);
 	}
-	
+
 	public void enterName(String name) {
 		seleniumUtills.entertext(payGradesObjects.name,name);
 	}
-	
+
 	public void clickOnSave() {
 		seleniumUtills.clickOnElement(payGradesObjects.save);
 	}
-	
-	
+
+	public void enterCurrency(String currency) {
+		seleniumUtills.entertext(payGradesObjects.currency,currency);
+	}
+
+	public void enterMinSalary(String minSalary) {
+		seleniumUtills.entertext(payGradesObjects.minSalary,minSalary);
+	}
+
+	public void enterMaxSalary(String maxSalary) {
+		seleniumUtills.entertext(payGradesObjects.maxSalary,maxSalary);
+	}
 
 }
