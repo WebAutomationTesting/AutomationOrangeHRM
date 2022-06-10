0975536433178452b9a217e714779015a393166c
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-10 13:00:31 +0530

    Updated Utils

--- src/main/java/com/module/utils/admin/job/JobCategoriesUtils.java
+++ src/main/java/com/module/utils/admin/job/JobCategoriesUtils.java
@@ -1,5 +1,41 @@
 package com.module.utils.admin.job;
 
-public class JobCategoriesUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.automation.utills.WaitUtils;
+import com.module.objects.admin.job.JobCategoriesObjects;
+
+public class JobCategoriesUtils extends BaseClass{
+	
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public WaitUtils waitUtils;
+	public JobCategoriesObjects jobCategoriesobjects;
+
+	public JobCategoriesUtils() {
+		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
+		jobCategoriesobjects=PageFactory.initElements(driver,JobCategoriesObjects.class);
+	}
+	
+	public void clickOnCategories() {
+		seleniumCore.clickOnSubSubmenu(jobCategoriesobjects.adminHeader, jobCategoriesobjects.jobLink, jobCategoriesobjects.jobCategory);
+	}
+	
+	public void addCategoriesDetails(String categoryName) {
+		seleniumUtills.entertext(jobCategoriesobjects.categoryName,categoryName);
+		
+	}
+	public void clickOnLogOut() {
+		seleniumUtills.clickOnElement(jobCategoriesobjects.logout);
+	}
+	public void clickOnWelcome() {
+		seleniumUtills.clickOnElement(jobCategoriesobjects.welcomeButton);
+	}
+	
+	
 }
