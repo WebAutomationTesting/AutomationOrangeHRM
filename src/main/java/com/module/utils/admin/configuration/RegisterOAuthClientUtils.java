3eab944a548c8275d68876d2ae3617e2d55746e5
HP <HP@DESKTOP-0TKDIHU>
2022-06-10 14:32:29 +0530

    updated client utils

--- src/main/java/com/module/utils/admin/configuration/RegisterOAuthClientUtils.java
+++ src/main/java/com/module/utils/admin/configuration/RegisterOAuthClientUtils.java
@@ -1,5 +1,43 @@
 package com.module.utils.admin.configuration;
 
-public class RegisterOAuthClientUtils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.admin.configuration.RegisterOAuthClientObjects;
+
+public class RegisterOAuthClientUtils extends BaseClass {
+	SeleniumCore seleniumCore;
+	SeleniumUtills seleniumUtills;
+	RegisterOAuthClientObjects registerOAuthClientObjects;
+
+	public RegisterOAuthClientUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		registerOAuthClientObjects=PageFactory.initElements(driver, RegisterOAuthClientObjects.class);
+
+	}
+	public void selectadmin() {
+		seleniumUtills.clickOnElement(registerOAuthClientObjects.admin);
+	}
+	public void selectconfiguration() {
+		seleniumUtills.clickOnElement(registerOAuthClientObjects.configuration);
+	}
+	public void selectregisteroauthclient() {
+		seleniumUtills.clickOnElement(registerOAuthClientObjects.registeroauthclient);
+	}
+	public void enterid( String id) {
+		seleniumUtills.entertext(registerOAuthClientObjects.id, currDir);
+	}
+	public void entersecert(String secert) {
+		seleniumUtills.entertext(registerOAuthClientObjects.secret, currDir);
+	}
+	public void enterredirecturi(String redirecturi) {
+		seleniumUtills.entertext(registerOAuthClientObjects.uri, currDir);
+	}
+	public void selectsave() {
+		seleniumUtills.clickOnElement(registerOAuthClientObjects.save);
+	}
 
 }
