374dfab70211fdc51e8721f9701424e59b8567c0
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-10 13:02:52 +0530

    Updated

--- src/main/java/com/module/utils/admin/job/WorkShiftsutils.java
+++ src/main/java/com/module/utils/admin/job/WorkShiftsutils.java
@@ -1,5 +1,45 @@
 package com.module.utils.admin.job;
 
-public class WorkShiftsutils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.automation.utills.WaitUtils;
+import com.module.objects.admin.job.JobCategoriesObjects;
+import com.module.objects.admin.job.WorkShiftsObjects;
+
+public class WorkShiftsutils extends BaseClass {
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public WaitUtils waitUtils;
+	public WorkShiftsObjects workShiftsObjects;
+	public JobCategoriesObjects jobCategoriesObjects;
+
+	public WorkShiftsutils() {
+		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
+		workShiftsObjects=PageFactory.initElements(driver,WorkShiftsObjects.class);
+		jobCategoriesObjects=PageFactory.initElements(driver,JobCategoriesObjects.class);
+	}
+
+	public void clickOnWorkShift() {
+		seleniumCore.clickOnSubSubmenu(jobCategoriesObjects.adminHeader,jobCategoriesObjects.jobLink, workShiftsObjects.workShiftsTab);
+	}
+	
+	public void AddWorkShiftDetails(String workShiftName) {
+		seleniumUtills.entertext(workShiftsObjects.workShiftName, workShiftName);	
+	}
+	public void selectFrom(String from) {
+		seleniumUtills.selectvisubleText(workShiftsObjects.workFrom,from);
+		waitUtils.waitTime(2000);
+		seleniumUtills.clickOnElement(workShiftsObjects.workOnTime);
+	}
+		public void selectTo(String to) {
+		seleniumUtills.selectvisubleText(workShiftsObjects.workTo,to);
+		waitUtils.waitTime(2000);
+		seleniumUtills.clickOnElement(workShiftsObjects.workOffTime);
+	}
 
 }
