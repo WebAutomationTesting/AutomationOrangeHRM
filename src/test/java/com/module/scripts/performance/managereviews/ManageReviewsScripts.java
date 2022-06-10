eca963b4f18eb4f242881c79d66c97634737e343
HP <HP@DESKTOP-3D45S6U>
2022-06-10 15:19:51 +0530

    Updated ManageReviewsScripts

--- src/test/java/com/module/scripts/performance/managereviews/ManageReviewsScripts.java
+++ src/test/java/com/module/scripts/performance/managereviews/ManageReviewsScripts.java
@@ -1,5 +1,61 @@
 package com.module.scripts.performance.managereviews;
 
-public class ManageReviewsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
+import com.module.objects.performance.managereviews.ManageReviewsObjects;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.performance.managereviews.ManageReviewsUtils;
+
+public class ManageReviewsScripts extends BaseClass{
+
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	ManageReviewsObjects manageReviewsObjects;
+	ManageReviewsUtils manageReviewsUtils;
+	WaitUtils waitUtils;
+
+	@BeforeClass
+	public void load () {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		manageReviewsObjects=PageFactory.initElements(driver, ManageReviewsObjects.class);
+		manageReviewsUtils=PageFactory.initElements(driver, ManageReviewsUtils.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
+
+	}
+
+	@Test
+	public void ManageReviewersDetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		manageReviewsUtils.clickonPerformance();
+		manageReviewsUtils.clickonManageReviews();
+		manageReviewsUtils.clickonSubManager();
+		waitUtils.waitTime(500);
+		manageReviewsUtils.enterTextEmployeename("Orange Text");
+		manageReviewsUtils.clickonJobTitle();
+		manageReviewsUtils.selectQaE();
+		manageReviewsUtils.clickonStatus();
+		manageReviewsUtils.clickonFromDate();
+		manageReviewsUtils.selectFromDate();
+		manageReviewsUtils.clickonToDate();
+		manageReviewsUtils.selectToDate();
+		manageReviewsUtils.clickonReviewer();
+		manageReviewsUtils.clickonSearch();
+		manageReviewsUtils.clickonAdd();
+		manageReviewsUtils.enterTextEployee("Orange Test");
+		manageReviewsUtils.selectEmployee();
+		waitUtils.waitTime(1000);
+		manageReviewsUtils.enterTextSupervisior("jack");
+		manageReviewsUtils.clickonWorkPeriodStart();
+		manageReviewsUtils.selectWorkPeriodStart();
+		manageReviewsUtils.clickonWorkPeriodEnd();
+		manageReviewsUtils.selectWorkPeriodEnd();
+		manageReviewsUtils.clickonDueDate();
+		manageReviewsUtils.selectDueDate();
+		manageReviewsUtils.clickonSave();
+		manageReviewsUtils.clickonActivate();
+	}
 
 }
