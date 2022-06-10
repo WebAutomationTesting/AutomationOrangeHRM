cb7e9a8e666d1b28f3ca2fbc44565170b0f42cda
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-10 12:55:19 +0530

    Updated objects

--- src/main/java/com/module/objects/admin/job/JobCategoriesObjects.java
+++ src/main/java/com/module/objects/admin/job/JobCategoriesObjects.java
@@ -1,5 +1,26 @@
 package com.module.objects.admin.job;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class JobCategoriesObjects {
+	
+	@FindBy(id ="jobCategory_name")
+	public WebElement categoryName;
+	
+	@FindBy(id ="menu_admin_Job")
+	public WebElement jobLink;
+	
+	@FindBy(id="menu_admin_jobCategory")
+	public WebElement jobCategory;
+	
+	@FindBy(id="menu_admin_viewAdminModule")
+	public WebElement adminHeader;
+	
+	@FindBy(xpath = "//a[text()='Logout']")
+	public WebElement logout;
+	
+	@FindBy(id="welcome")
+	public WebElement welcomeButton;
 
 }
