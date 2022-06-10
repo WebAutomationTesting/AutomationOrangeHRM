2abd0253ab743b05abb521176a96c3e1dde27266
HP <HP@DESKTOP-3D45S6U>
2022-06-10 15:21:26 +0530

    Updated MyReviewsUtils

--- src/main/java/com/module/utils/performance/managereviews/MyReviewsUtils.java
+++ src/main/java/com/module/utils/performance/managereviews/MyReviewsUtils.java
@@ -1,5 +1,33 @@
 package com.module.utils.performance.managereviews;
 
-public class MyReviewsUtils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.performance.managereviews.MyReviewsObjects;
+
+public class MyReviewsUtils extends BaseClass{
+
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public MyReviewsObjects myReviewsObjects;
+
+	public MyReviewsUtils() {
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		myReviewsObjects=PageFactory.initElements(driver, MyReviewsObjects.class);
+
+	}
+
+	public void clickonPerfomance() {
+		seleniumUtills.clickOnElement(myReviewsObjects.performance);
+	}
+	public  void clickonManageReviews() {
+		seleniumUtills.clickOnElement(myReviewsObjects.manageReviews);
+	}
+	public void clickonMyReviews() {
+		seleniumUtills.clickOnElement(myReviewsObjects.myReviews);
+	}
 
 }
