0b42d346d93c6651338a8bfc1737d7bf9a8c55ba
HP <HP@DESKTOP-0TKDIHU>
2022-06-10 14:31:47 +0530

    updated client objects

--- src/main/java/com/module/objects/admin/configuration/RegisterOAuthClientObjects.java
+++ src/main/java/com/module/objects/admin/configuration/RegisterOAuthClientObjects.java
@@ -1,5 +1,31 @@
 package com.module.objects.admin.configuration;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class RegisterOAuthClientObjects {
+
+	@FindBy(xpath = "//a[@id='menu_admin_viewAdminModule']")
+	public WebElement admin;
+
+	@FindBy(xpath ="//a[@id='menu_admin_Configuration']")
+	public WebElement configuration;
+
+	@FindBy(xpath = "//a[@id='menu_admin_registerOAuthClient']")
+	public WebElement registeroauthclient;
+
+	@FindBy(xpath = "//input[@id='oauth_client_id']")
+	public WebElement id;
+
+	@FindBy(xpath = "//input[@id='oauth_client_secret']")
+	public WebElement secret;
+
+	@FindBy(xpath = "//input[@id='oauth_redirect_uri']")
+	public WebElement uri;
+
+	@FindBy(xpath = "//input[@id='btnSave']" )
+	public WebElement save;
+
+
 
 }
