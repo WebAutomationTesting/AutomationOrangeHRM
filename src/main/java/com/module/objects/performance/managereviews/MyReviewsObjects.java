9742e512964aad2fbe40c72c516dc6baaf225256
HP <HP@DESKTOP-3D45S6U>
2022-06-10 15:20:49 +0530

    Updated MyReviewssObjects

--- src/main/java/com/module/objects/performance/managereviews/MyReviewsObjects.java
+++ src/main/java/com/module/objects/performance/managereviews/MyReviewsObjects.java
@@ -1,5 +1,18 @@
 package com.module.objects.performance.managereviews;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MyReviewsObjects {
+
+	@FindBy(id = "menu__Performance")
+	public WebElement performance;
+
+	@FindBy(id = "menu_performance_ManageReviews")
+	public WebElement manageReviews;
+
+	@FindBy(xpath = "//a[text()='My Reviews']")
+	public WebElement myReviews;
+
 
 }
