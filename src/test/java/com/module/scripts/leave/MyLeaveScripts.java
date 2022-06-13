c3bab0e18e36e118f9caccc29eac88d26ecb647f
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-13 12:44:36 +0530

    Update

--- src/test/java/com/module/scripts/leave/MyLeaveScripts.java
+++ src/test/java/com/module/scripts/leave/MyLeaveScripts.java
@@ -1,5 +1,35 @@
 package com.module.scripts.leave;
 
-public class MyLeaveScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.leave.MyLeaveUtils;
+
+public class MyLeaveScripts extends BaseClass{
+	
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	MyLeaveUtils myleaveUtils;
+	
+	@BeforeClass
+	public void loadClass() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		myleaveUtils=PageFactory.initElements(driver, MyLeaveUtils.class);
+		
+	}
+	
+	@Test
+	
+	public void myLeave() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		myleaveUtils.clickOnMyLeave();
+		myleaveUtils.myLeaveDetails("2022-01-02", "2022-12-30");
+		
+		
+	}
+	
+
+	
 }
