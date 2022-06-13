9e85fc5ca58eb2f179a8519b5eb8e9d9fd48503d
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-13 12:42:24 +0530

    Update

--- src/main/java/com/module/utils/leave/AssignLeaveUtils.java
+++ src/main/java/com/module/utils/leave/AssignLeaveUtils.java
@@ -1,5 +1,47 @@
 package com.module.utils.leave;
 
-public class AssignLeaveUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.automation.utills.WaitUtils;
+import com.module.objects.admin.usermanagement.UserspageObjects;
+import com.module.objects.leave.AssignLeaveobjects;
+
+public class AssignLeaveUtils extends BaseClass{
+
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public WaitUtils waitUtils;
+	public AssignLeaveobjects assignLeaveobjects;
+	UserspageObjects userspageObjects;
+
+	public  AssignLeaveUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
+		assignLeaveobjects=PageFactory.initElements(driver, AssignLeaveobjects.class);
+		userspageObjects=PageFactory.initElements(driver, UserspageObjects.class);
+	}
+
+	public void clickOnAssignleave() {
+		seleniumCore.clickOnSubmenu(assignLeaveobjects.leaveHeader, assignLeaveobjects.assignleaveLink);
+
+	}
+
+	public void assignLeaveDetails(String empName,String leaveType,String fromDate,String toDate,String comment) {
+		seleniumUtills.entertext(assignLeaveobjects.empName, empName);
+		waitUtils.waitTime(1000);
+		seleniumUtills.jsClick(userspageObjects.selectUser);
+		seleniumUtills.selectvisubleText(assignLeaveobjects.leaveType,leaveType);
+		seleniumUtills.entertext(assignLeaveobjects.fromDate, fromDate);
+		seleniumUtills.entertext(assignLeaveobjects.toDate, toDate);
+		seleniumUtills.entertext(assignLeaveobjects.comment, comment);
+
+	}
+	
+	public void clickOnassign() {
+		seleniumUtills.clickOnElement(assignLeaveobjects.assign);
+	}
 }
