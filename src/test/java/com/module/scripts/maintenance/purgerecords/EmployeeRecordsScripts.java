3ec86e79e0eafec5b94faf8754e726163d21d841
HP <HP@DESKTOP-3D45S6U>
2022-06-13 17:49:24 +0530

    Updated EmployeeScripts

--- src/test/java/com/module/scripts/maintenance/purgerecords/EmployeeRecordsScripts.java
+++ src/test/java/com/module/scripts/maintenance/purgerecords/EmployeeRecordsScripts.java
@@ -1,5 +1,31 @@
 package com.module.scripts.maintenance.purgerecords;
 
-public class EmployeeRecordsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.maintenance.purgerecords.EmployeeRecordsUtils;
+
+public class EmployeeRecordsScripts extends BaseClass{
+	
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	EmployeeRecordsUtils employeeRecordsutils;
+	
+	@BeforeClass
+	public void load() {
+				loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		employeeRecordsutils=PageFactory.initElements(driver, EmployeeRecordsUtils.class);
+	}
+	
+	@Test
+	public void EmployeeRecordsDetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		employeeRecordsutils.clickOnMaitanance();
+		employeeRecordsutils.clickOnPurgueRecords();
+		employeeRecordsutils.enterTextVerifyPassword("12345");
+		employeeRecordsutils.clickOnVerify();
+	}
 
 }
