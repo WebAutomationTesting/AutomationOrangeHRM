ba0d85377309a6dd8c58ee59e58cc403d2742732
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-13 12:45:02 +0530

    Update

--- src/test/java/com/module/scripts/leave/entitlements/AddEntitlementsScripts.java
+++ src/test/java/com/module/scripts/leave/entitlements/AddEntitlementsScripts.java
@@ -1,5 +1,41 @@
 package com.module.scripts.leave.entitlements;
 
-public class AddEntitlementsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.leave.entitlements.AddEntitlementsUtils;
+
+public class AddEntitlementsScripts extends BaseClass{
+
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	AddEntitlementsUtils addEntitlementsUtils;
+	WaitUtils waitUtils;
+
+	@BeforeClass
+	public void loadClass() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		addEntitlementsUtils=PageFactory.initElements(driver, AddEntitlementsUtils.class);
+		waitUtils=PageFactory.initElements(driver,WaitUtils.class);
+
+	}
+
+	@Test
+	public void addEntitlements() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		addEntitlementsUtils.clickOnAddEntitlements();
+		waitUtils.waitTime(1000);
+		addEntitlementsUtils.clickOnEmpName("Joe Root");
+		waitUtils.waitTime(1000);
+		addEntitlementsUtils.selectOnLeavetype("US - Bereavement");
+		waitUtils.waitTime(1000);
+		addEntitlementsUtils.selectOnLeaveperiod("2022-01-01 - 2022-12-31");
+		waitUtils.waitTime(1000);
+		addEntitlementsUtils.enterEntitlement("20");
+		addEntitlementsUtils.clicksave();
+	}
 
 }
