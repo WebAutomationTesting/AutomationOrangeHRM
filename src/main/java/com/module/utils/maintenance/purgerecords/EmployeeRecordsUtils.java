d3fc6be08e0ec96084aae031fbcd8ea312997ca8
HP <HP@DESKTOP-3D45S6U>
2022-06-13 17:47:22 +0530

    Updated EmployeeRecordUtils

--- src/main/java/com/module/utils/maintenance/purgerecords/EmployeeRecordsUtils.java
+++ src/main/java/com/module/utils/maintenance/purgerecords/EmployeeRecordsUtils.java
@@ -1,5 +1,35 @@
 package com.module.utils.maintenance.purgerecords;
 
-public class EmployeeRecordsUtils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.maintenance.purgerecords.EmployeeRecordsObjects;
+
+public class EmployeeRecordsUtils extends BaseClass{
+	
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public EmployeeRecordsObjects employeeRecordsObjects;
+	
+	public EmployeeRecordsUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		employeeRecordsObjects=PageFactory.initElements(driver, EmployeeRecordsObjects.class);
+	}
+	public void clickOnMaitanance() {
+		seleniumUtills.clickOnElement(employeeRecordsObjects.maintanance);
+	}
+	public void clickOnPurgueRecords() {
+		seleniumUtills.clickOnElement(employeeRecordsObjects.purgeEmployee);
+		seleniumUtills.clickOnElement(employeeRecordsObjects.employeeRecord);
+	}
+	public void enterTextVerifyPassword(String name) {
+		seleniumUtills.entertext(employeeRecordsObjects.password, name);
+	}
+	public void clickOnVerify() {
+		seleniumUtills.clickOnElement(employeeRecordsObjects.verify);
+	}
 
 }
