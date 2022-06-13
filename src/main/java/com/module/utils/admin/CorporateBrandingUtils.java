71a9ac7c21437931d97aafc523bfdcf9f3521f12
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-13 18:42:49 +0530

    Updated

--- src/main/java/com/module/utils/admin/CorporateBrandingUtils.java
+++ src/main/java/com/module/utils/admin/CorporateBrandingUtils.java
@@ -1,5 +1,46 @@
 package com.module.utils.admin;
 
-public class CorporateBrandingUtils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.admin.CorporateBrandingObjects;
+import com.module.objects.admin.job.JobCategoriesObjects;
+
+public class CorporateBrandingUtils extends BaseClass{
+
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public CorporateBrandingObjects corporateBrandingObjects;
+	public JobCategoriesObjects jobCategoriesobjects;
+
+	public CorporateBrandingUtils() {
+		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		corporateBrandingObjects=PageFactory.initElements(driver,CorporateBrandingObjects.class);
+		jobCategoriesobjects=PageFactory.initElements(driver,JobCategoriesObjects.class);
+	}
+
+	public void clickOnCorporateBranding() {
+		seleniumCore.clickOnSubmenu(jobCategoriesobjects.adminHeader,corporateBrandingObjects.corporateBranding);
+	}
+
+	public void clickOnPrimary() {
+		seleniumUtills.clickOnElement(corporateBrandingObjects.primaryColor);
+	}
+	public void clickOnChooseColor() {	
+		seleniumUtills.clickOnElement(corporateBrandingObjects.chooseColor);
+	}
+	public void clickOnsecondaryColor() {
+		seleniumUtills.clickOnElement(corporateBrandingObjects.secondaryColor);
+	}
+	public void clickOnChoose() {
+		seleniumUtills.clickOnElement(corporateBrandingObjects.color);
+	}
+
+	public void clickOnPublish() {
+		seleniumUtills.clickOnElement(corporateBrandingObjects.publish);
+	}
 
 }
