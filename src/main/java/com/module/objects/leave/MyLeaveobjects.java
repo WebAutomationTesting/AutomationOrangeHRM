d1b40688e74b6255b8628695936e864406ef515d
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-13 12:40:35 +0530

    Update

--- src/main/java/com/module/objects/leave/MyLeaveobjects.java
+++ src/main/java/com/module/objects/leave/MyLeaveobjects.java
@@ -1,5 +1,23 @@
 package com.module.objects.leave;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MyLeaveobjects {
+	
+	@FindBy(id="menu_leave_viewLeaveModule")
+	public WebElement leaveHeader;
+	
+	@FindBy(id="menu_leave_viewMyLeaveList")
+	public WebElement myleaveLink;
+	
+	@FindBy(id="calFromDate")
+	public WebElement fromDate;
+	
+	@FindBy(id="calToDate")
+	public WebElement toDate;
+	
+	@FindBy(id="btnSearch")
+	public WebElement search;
 
 }
