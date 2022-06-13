7f954ed19be4e902ff27c9fb4a285909438dd058
HP <HP@DESKTOP-3D45S6U>
2022-06-13 11:58:04 +0530

    Updated BuzzObjects

--- src/main/java/com/module/objects/buzz/BuzzObjects.java
+++ src/main/java/com/module/objects/buzz/BuzzObjects.java
@@ -1,5 +1,20 @@
 package com.module.objects.buzz;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class BuzzObjects {
+
+	@FindBy(id = "menu_buzz_viewBuzz")
+	public WebElement buzz;
+
+	@FindBy(id = "status-tab-label")
+	public WebElement upDateStatus;
+
+	@FindBy(id = "createPost_content")
+	public WebElement status;
+
+	@FindBy(id = "postSubmitBtn")
+	public WebElement post;
 
 }
