5785534782aac55e4ecf60659ecb18314b2dfa84
HP <HP@DESKTOP-3D45S6U>
2022-06-13 17:48:49 +0530

    Updated CandidateRecordsScripts

--- src/test/java/com/module/scripts/maintenance/purgerecords/CandidateRecordsScripts.java
+++ src/test/java/com/module/scripts/maintenance/purgerecords/CandidateRecordsScripts.java
@@ -1,5 +1,35 @@
 package com.module.scripts.maintenance.purgerecords;
 
-public class CandidateRecordsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.WaitUtils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.maintenance.purgerecords.CandidateRecordsUtils;
+
+public class CandidateRecordsScripts extends BaseClass{
+
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	CandidateRecordsUtils candidateRecordsUtils;
+	WaitUtils waitUtils;
+
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		candidateRecordsUtils=PageFactory.initElements(driver, CandidateRecordsUtils.class);
+		waitUtils=PageFactory.initElements(driver, WaitUtils.class);
+	}
+
+	@Test
+	public void CandidateRecordsDetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		candidateRecordsUtils.clickOnMaitanance();
+		candidateRecordsUtils.clickOnPurgeRecords();
+		waitUtils.waitTime(500);
+		candidateRecordsUtils.enterTextPassword("1234");
+		candidateRecordsUtils.clickOnVerify();
+	}
 
 }
