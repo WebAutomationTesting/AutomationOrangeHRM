0cb53fb24543b82059c5725ceff09d722a56164c
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-13 18:41:05 +0530

    UpdatedObjects

--- src/main/java/com/module/objects/admin/CorporateBrandingObjects.java
+++ src/main/java/com/module/objects/admin/CorporateBrandingObjects.java
@@ -1,5 +1,31 @@
 package com.module.objects.admin;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CorporateBrandingObjects {
+
+	@FindBy(id="menu_admin_addTheme")
+	public WebElement corporateBranding;
+
+	@FindBy(xpath = "(//div[@class='sp-dd'])[1]")
+	public WebElement primaryColor;
+
+	@FindBy(xpath = "(//div[@class='sp-color'])[1]")
+	public WebElement chooseColor;
+
+	@FindBy(xpath = "(//button[text()='choose'])[1]")
+	public WebElement color;
+
+	@FindBy(xpath = "(//div[@class='sp-dd'])[2]")
+	public WebElement secondaryColor;
+
+	@FindBy(xpath = "(//div[@class='sp-hue'])[1]")
+	public WebElement secondChoose;
+
+	@FindBy(id="btnSave")
+	public WebElement publish;
+
+
 
 }
