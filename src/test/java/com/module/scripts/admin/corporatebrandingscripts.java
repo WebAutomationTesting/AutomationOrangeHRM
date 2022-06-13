6ed7cf0d6726d2efe4096a33d83205332954bc4c
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-13 18:44:25 +0530

    Updated Scripts

--- src/test/java/com/module/scripts/admin/corporatebrandingscripts.java
+++ src/test/java/com/module/scripts/admin/corporatebrandingscripts.java
@@ -1,5 +1,40 @@
 package com.module.scripts.admin;
 
-public class corporatebrandingscripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseClass;
+import com.module.objects.homepage.LoginLogoutpageObjects;
+import com.module.utils.admin.CorporateBrandingUtils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+
+public class corporatebrandingscripts extends BaseClass {
+
+	CorporateBrandingUtils corporateBrandingUtils;
+	LoginLogoutpageObjects loginLogoutpageObject;
+	LoginLogoutpageUtils  loginLogoutpageUtils;
+
+	@BeforeClass
+	public void load() {
+		corporateBrandingUtils=PageFactory.initElements(driver,CorporateBrandingUtils.class);
+		loginLogoutpageObject=PageFactory.initElements(driver,LoginLogoutpageObjects.class);	
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+
+	}
+
+	@Test
+	public void corporateBrandingdetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin","admin123");
+		corporateBrandingUtils.clickOnCorporateBranding();
+		corporateBrandingUtils.clickOnPrimary();
+		corporateBrandingUtils.clickOnChooseColor();
+		corporateBrandingUtils.clickOnChoose();
+		corporateBrandingUtils.clickOnPublish();
+
+
+	}
+
+
 
 }
