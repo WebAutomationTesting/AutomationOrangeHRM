4e8aef0917139df77a79b655a7735105458fc30a
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-13 15:24:57 +0530

    Updated Objects

--- src/main/java/com/module/objects/admin/NationalitiesObjects.java
+++ src/main/java/com/module/objects/admin/NationalitiesObjects.java
@@ -1,5 +1,21 @@
 package com.module.objects.admin;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class NationalitiesObjects {
+	
+	@FindBy(id="menu_admin_nationality")
+	public WebElement nationality;
+	
+	@FindBy(id="btnAdd")
+	public WebElement add;
+	
+	@FindBy(id="nationality_name")
+	public WebElement addNationalityName;
+	
+	@FindBy(id="btnSave")
+	public WebElement save;
+	
 
 }
