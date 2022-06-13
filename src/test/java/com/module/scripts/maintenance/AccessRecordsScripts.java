07fff11b5b375528ae43cd2b577e9d8812067039
HP <HP@DESKTOP-3D45S6U>
2022-06-13 17:48:02 +0530

    Updated AccessRecordsScripts

--- src/test/java/com/module/scripts/maintenance/AccessRecordsScripts.java
+++ src/test/java/com/module/scripts/maintenance/AccessRecordsScripts.java
@@ -1,5 +1,30 @@
 package com.module.scripts.maintenance;
 
-public class AccessRecordsScripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+import com.module.utils.maintenance.AccessRecordsUtils;
+
+public class AccessRecordsScripts extends BaseClass{
+
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	AccessRecordsUtils accessRecordsutils;
+
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		accessRecordsutils=PageFactory.initElements(driver, AccessRecordsUtils.class);
+	}
+
+	@Test
+	public void AccessRecordDetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		accessRecordsutils.clickOnMaitanance();
+		accessRecordsutils.clickOnAccess();
+		accessRecordsutils.enterTextPassword("1234");
+		accessRecordsutils.clickOnVerify();
+	}
 }
