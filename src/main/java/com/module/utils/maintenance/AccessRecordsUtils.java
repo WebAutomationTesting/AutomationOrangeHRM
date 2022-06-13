0b42f28ce100b439a581cdaf37970387362e3f99
HP <HP@DESKTOP-3D45S6U>
2022-06-13 17:46:11 +0530

    Updated AccessRecordsUtils

--- src/main/java/com/module/utils/maintenance/AccessRecordsUtils.java
+++ src/main/java/com/module/utils/maintenance/AccessRecordsUtils.java
@@ -1,5 +1,34 @@
 package com.module.utils.maintenance;
 
-public class AccessRecordsUtils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.maintenance.accessrecords.AccessRecordsObjects;
+
+public class AccessRecordsUtils extends BaseClass{
+
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public AccessRecordsObjects accessRecordsObjects;
+
+	public AccessRecordsUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		accessRecordsObjects=PageFactory.initElements(driver, AccessRecordsObjects.class);
+	}
+	public void clickOnMaitanance() {
+		seleniumUtills.clickOnElement(accessRecordsObjects.maintanance);
+	}
+	public void clickOnAccess() {
+		seleniumUtills.clickOnElement(accessRecordsObjects.accessRecord);
+	}
+	public void enterTextPassword(String name) {
+		seleniumUtills.entertext(accessRecordsObjects.password, name);
+	}
+	public void clickOnVerify() {
+		seleniumUtills.clickOnElement(accessRecordsObjects.verify);
+	}
 
 }
