7e909a4f1af2299e3c6c9cd4788f9a6c2b71094a
gopikrishnasettipalli <gopikrishnasettipalli@localhost>
2022-06-13 15:26:57 +0530

    UpdatedUtils

--- src/main/java/com/module/utils/admin/NationalitiesUtils.java
+++ src/main/java/com/module/utils/admin/NationalitiesUtils.java
@@ -1,5 +1,37 @@
 package com.module.utils.admin;
 
-public class NationalitiesUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.admin.NationalitiesObjects;
+import com.module.objects.admin.job.JobCategoriesObjects;
+
+public class NationalitiesUtils extends BaseClass{
+	
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public NationalitiesObjects nationalitiesObjects;
+	public JobCategoriesObjects jobCategoriesobjects;
+
+	public NationalitiesUtils() {
+		seleniumUtills=PageFactory.initElements(driver,SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver,SeleniumCore.class);
+		nationalitiesObjects=PageFactory.initElements(driver,NationalitiesObjects.class);
+		jobCategoriesobjects=PageFactory.initElements(driver,JobCategoriesObjects.class);
+	}
+	
+	public void clickOnNationality() {
+		seleniumCore.clickOnSubmenu(jobCategoriesobjects.adminHeader,nationalitiesObjects.nationality);
+	}
+	
+	public void enterName(String name) {
+		seleniumUtills.entertext(nationalitiesObjects.addNationalityName,name);
+		
+	}
+	public void clickOnSave() {
+		seleniumUtills.clickOnElement(nationalitiesObjects.save);
+	}
+	
 }
