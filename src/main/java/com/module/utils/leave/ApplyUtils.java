a8e25e40ca18a618306571fbdfc223f5ee21e012
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-13 12:48:22 +0530

    Update

--- src/main/java/com/module/utils/leave/ApplyUtils.java
+++ src/main/java/com/module/utils/leave/ApplyUtils.java
@@ -1,5 +1,38 @@
 package com.module.utils.leave;
 
-public class ApplyUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.automation.utills.WaitUtils;
+import com.module.objects.leave.ApplyObjects;
+
+public class ApplyUtils extends BaseClass{
+
+
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public WaitUtils waitUtils;
+	public ApplyObjects applyObjects;
+
+
+	public ApplyUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		applyObjects=PageFactory.initElements(driver, ApplyObjects.class);
+
+	}
+
+	public void clickOnApply() {
+		seleniumCore.clickOnSubmenu(applyObjects.leaveHeader,applyObjects.applylink);
+			
+	}
+	
+	public void applyDetails() {
+		seleniumUtills.clickOnElement(applyObjects.leaveHeader);
+		seleniumUtills.clickOnElement(applyObjects.applylink);
+		
+	}
+	
 }
