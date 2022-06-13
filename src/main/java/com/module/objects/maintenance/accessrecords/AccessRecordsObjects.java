c94eb00a0d24289dbd808a7172d54c3ec6a4fb0b
HP <HP@DESKTOP-3D45S6U>
2022-06-13 17:33:51 +0530

    Updated AccessRecordsObjects

--- src/main/java/com/module/objects/maintenance/accessrecords/AccessRecordsObjects.java
+++ src/main/java/com/module/objects/maintenance/accessrecords/AccessRecordsObjects.java
@@ -1,5 +1,20 @@
 package com.module.objects.maintenance.accessrecords;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AccessRecordsObjects {
+
+	@FindBy(id = "menu_maintenance_purgeEmployee")
+	public WebElement maintanance;
+
+	@FindBy(id = "menu_maintenance_accessEmployeeData")
+	public WebElement accessRecord;
+
+	@FindBy(id = "confirm_password")
+	public WebElement password;
+
+	@FindBy(xpath = "//input[@type='submit']")
+	public WebElement verify;
 
 }
