2942f9471bd8bad97837fc83de5452a6b54a77c3
HP <HP@DESKTOP-3D45S6U>
2022-06-13 11:58:39 +0530

    Updated BuzzUtils

--- src/main/java/com/module/utils/buzz/BuzzUtils.java
+++ src/main/java/com/module/utils/buzz/BuzzUtils.java
@@ -1,5 +1,35 @@
 package com.module.utils.buzz;
 
-public class BuzzUtils {
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.buzz.BuzzObjects;
+
+public class BuzzUtils extends BaseClass{
+
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public BuzzObjects buzzObjects;
+
+	public BuzzUtils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		buzzObjects=PageFactory.initElements(driver, BuzzObjects.class);
+	}
+
+	public void clickOnBuzz() {
+		seleniumUtills.clickOnElement(buzzObjects.buzz);		
+	}
+	public void clickOnUpDateStatus() {
+		seleniumUtills.clickOnElement(buzzObjects.upDateStatus);
+	}
+	public void enterTextStatus(String name) {
+		seleniumUtills.entertext(buzzObjects.status, name);
+	}
+	public void clickOnPost() {
+		seleniumUtills.clickOnElement(buzzObjects.post);
+	}
 
 }
