87e36b12b593d5aab9e252a63cdd1cd5f9d09a53
lenovo <lenovo@DESKTOP-M51RO1K>
2022-06-14 14:52:36 +0530

    Update

--- src/main/java/com/module/utils/leave/entitlements/EmployeeEntitlementsUtils.java
+++ src/main/java/com/module/utils/leave/entitlements/EmployeeEntitlementsUtils.java
@@ -1,5 +1,46 @@
 package com.module.utils.leave.entitlements;
 
-public class EmployeeEntitlementsUtils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.leave.entitlements.EmployeeEntitlementsObjects;
+
+public class EmployeeEntitlementsUtils extends BaseClass{
+
+	public SeleniumUtills seleniumUtills;
+	public SeleniumCore seleniumCore;
+	public EmployeeEntitlementsObjects employeeEntitlementsObjects;
+
+
+	public EmployeeEntitlementsUtils() {
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		employeeEntitlementsObjects=PageFactory.initElements(driver, EmployeeEntitlementsObjects.class);
+
+	}
+	public void clickOnEmployeeEntitlements() {
+		seleniumCore.clickOnSubSubmenu(employeeEntitlementsObjects.leaveHeader, employeeEntitlementsObjects.entitlementsSubHeader,employeeEntitlementsObjects.employeeEntitlementsLink);
+
+	}
+	public void clickOnEmployeeName(String employeeName) {
+		seleniumUtills.entertext(employeeEntitlementsObjects.employeeName,employeeName);
+
+	}
+	public void selectOnLeaveType(String type) {
+		seleniumUtills.selectvisubleText(employeeEntitlementsObjects.leaveType, type);
+		
+	}
+	public void selectOnLeavePeriod(String period) {
+		seleniumUtills.selectvisubleText(employeeEntitlementsObjects.leavePeriod, period);
+		
+	}
+
+	public void clickOnSearch() {
+		seleniumUtills.clickOnElement(employeeEntitlementsObjects.search);
+	}
+	
 }
+
+
