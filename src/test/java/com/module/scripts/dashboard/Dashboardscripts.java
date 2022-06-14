89803ac59330c8c2f688733568e08a5fff85396c
HP <HP@DESKTOP-3D45S6U>
2022-06-14 14:46:42 +0530

    Updated DashboardScripts

--- src/test/java/com/module/scripts/dashboard/Dashboardscripts.java
+++ src/test/java/com/module/scripts/dashboard/Dashboardscripts.java
@@ -1,5 +1,27 @@
 package com.module.scripts.dashboard;
 
-public class Dashboardscripts {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import com.automation.base.BaseClass;
+import com.module.utils.dashboard.Dashboardutils;
+import com.module.utils.homepage.LoginLogoutpageUtils;
+
+public class Dashboardscripts extends BaseClass{
+
+	LoginLogoutpageUtils loginLogoutpageUtils;
+	Dashboardutils dashboardutils;
+
+	@BeforeClass
+	public void load() {
+		loginLogoutpageUtils=PageFactory.initElements(driver, LoginLogoutpageUtils.class);
+		dashboardutils=PageFactory.initElements(driver, Dashboardutils.class);
+	}
+
+	@Test
+	public void dashboardDetails() {
+		loginLogoutpageUtils.applicationCredentials("Admin", "admin123");
+		dashboardutils.clickOnDashboard();
+	}
 }
