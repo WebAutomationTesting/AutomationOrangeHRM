cbd3c828617fe9a4db510042e9aaabefe3d58c09
HP <HP@DESKTOP-3D45S6U>
2022-06-14 14:44:41 +0530

    Updated DashboardUtils

--- src/main/java/com/module/utils/dashboard/Dashboardutils.java
+++ src/main/java/com/module/utils/dashboard/Dashboardutils.java
@@ -1,5 +1,25 @@
 package com.module.utils.dashboard;
 
-public class Dashboardutils {
+import org.openqa.selenium.support.PageFactory;
 
+import com.automation.base.BaseClass;
+import com.automation.utills.SeleniumCore;
+import com.automation.utills.SeleniumUtills;
+import com.module.objects.admin.dashboard.DashboardObjects;
+
+public class Dashboardutils extends BaseClass{
+
+	public SeleniumCore seleniumCore;
+	public SeleniumUtills seleniumUtills;
+	public DashboardObjects dashboardObjects;
+
+	public Dashboardutils() {
+		seleniumCore=PageFactory.initElements(driver, SeleniumCore.class);
+		seleniumUtills=PageFactory.initElements(driver, SeleniumUtills.class);
+		dashboardObjects=PageFactory.initElements(driver, DashboardObjects.class);
+	}
+
+	public void clickOnDashboard() {
+		seleniumUtills.clickOnElement(dashboardObjects.dashboard);
+	}
 }
