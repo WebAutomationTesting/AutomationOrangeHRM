1f2987b4afdb9bc24ab3ba01ebb090db7a2b6cdf
HP <HP@DESKTOP-3D45S6U>
2022-06-14 14:43:23 +0530

    Updated DashboardObjects

--- src/main/java/com/module/objects/admin/dashboard/DashboardObjects.java
+++ src/main/java/com/module/objects/admin/dashboard/DashboardObjects.java
@@ -1,5 +1,11 @@
 package com.module.objects.admin.dashboard;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class DashboardObjects {
+	
+	@FindBy(id = "menu_dashboard_index")
+	public WebElement dashboard;
 
 }
